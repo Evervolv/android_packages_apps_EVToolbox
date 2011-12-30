5fbdab9b4c8a294f1b5e68932fd6376cf06c61a8
andrew.boren <andrew.boren@gmail.com>
2011-12-29 22:13:52 -0700

    EVToolbox: Add lockscreen style options.
    Change-Id: Id9822c8762831765517e3df71863b30c8fea596a

--- src/com/evervolv/toolbox/activities/Lockscreen.java
+++ src/com/evervolv/toolbox/activities/Lockscreen.java
@@ -2,12 +2,25 @@ package com.evervolv.toolbox.activities;
 
 import android.content.ContentResolver;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
 
 import com.evervolv.toolbox.R;
 import com.evervolv.toolbox.SettingsFragment;
 
-public class Lockscreen extends SettingsFragment {
+public class Lockscreen extends SettingsFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String LOCKSCREEN_STYLE_PREF = "pref_lockscreen_settings_style";
+
+    private static final int LOCK_STYLE_GB   = 1;
+    private static final int LOCK_STYLE_ECLAIR = 2;
+    private static final int LOCK_STYLE_ICS = 3;
+    private ListPreference mLockscreenStyle;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +31,18 @@ public class Lockscreen extends SettingsFragment {
         ContentResolver cr = getContentResolver();
         
         
+        /* Lockscreen style */
+        mLockscreenStyle = (ListPreference) prefSet.findPreference(LOCKSCREEN_STYLE_PREF);
+        mLockscreenStyle.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLockscreenStyle) {
+            Settings.System.putInt(getContentResolver(), Settings.System.LOCKSCREEN_STYLE,
+                    Integer.valueOf((String) newValue));
+            return true;
+        }
+        return false;
     }
 }
