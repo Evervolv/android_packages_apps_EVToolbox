ea04cc2c39f5c357c9dc8e076028c590e5f4d0cb
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-01-13 22:05:28 -0600

    EVToolbox: volume button wake toggle
    moved vol music controls into interface/buttons
    volume wake disables the ability to use music controls
    (this happens even on cm7)
    so i made the options disable each other when checked
    to try and prevent user confusion when using vol wake
    music control is still default on
    volume wake defaults to off
    
    Change-Id: I45328276ac3b010a7efd751768785bc91a2b77d7

--- src/com/evervolv/toolbox/activities/Lockscreen.java
+++ src/com/evervolv/toolbox/activities/Lockscreen.java
@@ -14,14 +14,12 @@ import com.evervolv.toolbox.activities.subactivities.LockscreenStyle;
 
 public class Lockscreen extends SettingsFragment {
 
-    private static final String LOCKSCREEN_MUSIC_CTRL_VOLBTN = "pref_lockscreen_music_controls_volbtn";
     private static final String LOCKSCREEN_STYLE_PREF = "pref_lockscreen_style";
 
     private static final String TAG = "EVToolbox";
 
     private ContentResolver mCr;
     private PreferenceScreen mPrefSet;
-    private CheckBoxPreference mLockscreenMusicCtrlVolBtnPref;
     private PreferenceScreen mLockStyle;
 
     @Override
@@ -34,22 +32,11 @@ public class Lockscreen extends SettingsFragment {
         mCr = getContentResolver();
         mLockStyle = (PreferenceScreen) mPrefSet.findPreference(
                 LOCKSCREEN_STYLE_PREF);
- 
-        /* Volume button music controls */
-        mLockscreenMusicCtrlVolBtnPref = (CheckBoxPreference) mPrefSet.findPreference(LOCKSCREEN_MUSIC_CTRL_VOLBTN);
-        mLockscreenMusicCtrlVolBtnPref.setChecked(Settings.System.getInt(getContentResolver(),
-                Settings.System.LOCKSCREEN_MUSIC_CONTROLS_VOLBTN, 1) == 1);
     }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        boolean value;
-        if (preference == mLockscreenMusicCtrlVolBtnPref) {
-            value = mLockscreenMusicCtrlVolBtnPref.isChecked();
-            Settings.System.putInt(mCr,
-                    Settings.System.LOCKSCREEN_MUSIC_CONTROLS_VOLBTN, value ? 1 : 0);
-            return true;
-        } else if (preference == mLockStyle) {
+        if (preference == mLockStyle) {
             startPreferencePanel(mLockStyle.getFragment(),
                     null, mLockStyle.getTitleRes(), null, null, -1);
             return true;
--- src/com/evervolv/toolbox/activities/subactivities/InterfaceButtons.java
+++ src/com/evervolv/toolbox/activities/subactivities/InterfaceButtons.java
@@ -14,13 +14,16 @@ import com.evervolv.toolbox.SettingsFragment;
 public class InterfaceButtons extends SettingsFragment {
 
     private static final String TRACKBALL_WAKE_TOGGLE = "pref_trackball_wake_toggle";
-
     private static final String BUTTON_CATEGORY = "pref_category_button_settings";
+    private static final String VOLUME_WAKE_TOGGLE = "pref_volume_wake_toggle";
+    private static final String LOCKSCREEN_MUSIC_CTRL_VOLBTN = "pref_lockscreen_music_controls_volbtn";
 
     private ContentResolver mCr;
     private PreferenceScreen mPrefSet;
 
     private CheckBoxPreference mTrackballWake;
+    private CheckBoxPreference mVolumeWake;
+    private CheckBoxPreference mMusicCtrlVolBtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,15 +34,26 @@ public class InterfaceButtons extends SettingsFragment {
         mPrefSet = getPreferenceScreen();
         mCr = getContentResolver();
 
+        PreferenceCategory buttonCategory = (PreferenceCategory) mPrefSet
+                .findPreference(BUTTON_CATEGORY);
+
         /* Trackball wake pref */
         mTrackballWake = (CheckBoxPreference) mPrefSet.findPreference(
                 TRACKBALL_WAKE_TOGGLE);
         mTrackballWake.setChecked(Settings.System.getInt(getContentResolver(),
                 Settings.System.TRACKBALL_WAKE_SCREEN, 1) == 1);
 
-        PreferenceCategory buttonCategory = (PreferenceCategory) mPrefSet
-                .findPreference(BUTTON_CATEGORY);
+        /* Volume wake pref */
+        mVolumeWake = (CheckBoxPreference) mPrefSet.findPreference(VOLUME_WAKE_TOGGLE);
+        mVolumeWake.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.VOLUME_WAKE_SCREEN, 0) == 1);
 
+        /* Volume button music controls */
+        mMusicCtrlVolBtn = (CheckBoxPreference) mPrefSet.findPreference(LOCKSCREEN_MUSIC_CTRL_VOLBTN);
+        mMusicCtrlVolBtn.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKSCREEN_MUSIC_CONTROLS_VOLBTN, 1) == 1);
+
+        /* Remove mTrackballWake on devices without trackballs */
         if (!getResources().getBoolean(R.bool.has_trackball)) {
             buttonCategory.removePreference(mTrackballWake);
         }
@@ -50,7 +64,26 @@ public class InterfaceButtons extends SettingsFragment {
         boolean value;
         if (preference == mTrackballWake) {
             value = mTrackballWake.isChecked();
-            Settings.System.putInt(mCr, Settings.System.TRACKBALL_WAKE_SCREEN, value ? 1 : 0);
+            Settings.System.putInt(mCr, Settings.System.TRACKBALL_WAKE_SCREEN,
+                    value ? 1 : 0);
+            return true;
+        } else if (preference == mVolumeWake) {
+            value = mVolumeWake.isChecked();
+            Settings.System.putInt(mCr, Settings.System.VOLUME_WAKE_SCREEN,
+                    value ? 1 : 0);
+            if (mVolumeWake.isChecked()) {
+                mMusicCtrlVolBtn.setChecked(false);
+                Settings.System.putInt(mCr, Settings.System.LOCKSCREEN_MUSIC_CONTROLS_VOLBTN, 0);
+            }
+            return true;
+        } else if (preference == mMusicCtrlVolBtn) {
+            value = mMusicCtrlVolBtn.isChecked();
+            Settings.System.putInt(mCr, Settings.System.LOCKSCREEN_MUSIC_CONTROLS_VOLBTN,
+                    value ? 1 : 0);
+            if (mMusicCtrlVolBtn.isChecked()) {
+                mVolumeWake.setChecked(false);
+                Settings.System.putInt(mCr, Settings.System.VOLUME_WAKE_SCREEN, 0);
+            }
             return true;
         }
         return false;
