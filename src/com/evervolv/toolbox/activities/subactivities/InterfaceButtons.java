75006dde3a92d0de7e2cf03798fa336108acc143
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-02-23 00:14:23 +0000

    evtoolbox: new menu: performance
    kang of part of cm's memorymanagement and processor menus
    adds a bootreciever to enable ksm / manage cpu settings
    
    fix up the interface settings a bit: remove uneeded buttoncategory
    use preference screen to remove trackballwake toggle.
    
    Change-Id: I09c6f831663d9aefe4fbf2aa0658c31bc48a7cca

--- src/com/evervolv/toolbox/activities/subactivities/InterfaceButtons.java
+++ src/com/evervolv/toolbox/activities/subactivities/InterfaceButtons.java
@@ -14,7 +14,6 @@ import com.evervolv.toolbox.SettingsFragment;
 public class InterfaceButtons extends SettingsFragment {
 
     private static final String TRACKBALL_WAKE_TOGGLE = "pref_trackball_wake_toggle";
-    private static final String BUTTON_CATEGORY = "pref_category_button_settings";
     private static final String VOLUME_WAKE_TOGGLE = "pref_volume_wake_toggle";
     private static final String LOCKSCREEN_MUSIC_CTRL_VOLBTN = "pref_lockscreen_music_controls_volbtn";
 
@@ -34,28 +33,26 @@ public class InterfaceButtons extends SettingsFragment {
         mPrefSet = getPreferenceScreen();
         mCr = getContentResolver();
 
-        PreferenceCategory buttonCategory = (PreferenceCategory) mPrefSet
-                .findPreference(BUTTON_CATEGORY);
 
         /* Trackball wake pref */
         mTrackballWake = (CheckBoxPreference) mPrefSet.findPreference(
                 TRACKBALL_WAKE_TOGGLE);
-        mTrackballWake.setChecked(Settings.System.getInt(getContentResolver(),
+        mTrackballWake.setChecked(Settings.System.getInt(mCr,
                 Settings.System.TRACKBALL_WAKE_SCREEN, 1) == 1);
 
         /* Volume wake pref */
         mVolumeWake = (CheckBoxPreference) mPrefSet.findPreference(VOLUME_WAKE_TOGGLE);
-        mVolumeWake.setChecked(Settings.System.getInt(getContentResolver(),
+        mVolumeWake.setChecked(Settings.System.getInt(mCr,
                 Settings.System.VOLUME_WAKE_SCREEN, 0) == 1);
 
         /* Volume button music controls */
         mMusicCtrlVolBtn = (CheckBoxPreference) mPrefSet.findPreference(LOCKSCREEN_MUSIC_CTRL_VOLBTN);
-        mMusicCtrlVolBtn.setChecked(Settings.System.getInt(getContentResolver(),
+        mMusicCtrlVolBtn.setChecked(Settings.System.getInt(mCr,
                 Settings.System.LOCKSCREEN_MUSIC_CONTROLS_VOLBTN, 1) == 1);
 
         /* Remove mTrackballWake on devices without trackballs */
         if (!getResources().getBoolean(R.bool.has_trackball)) {
-            buttonCategory.removePreference(mTrackballWake);
+            mPrefSet.removePreference(mTrackballWake);
         }
 
     }
