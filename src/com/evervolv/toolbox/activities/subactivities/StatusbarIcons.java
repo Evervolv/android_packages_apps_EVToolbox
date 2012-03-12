55b486550b53318588fcc8f6f2d6df760256d5a3
Paul Mongold <pmongold@gmail.com>
2012-03-11 21:06:57 -0700

    option to choose battery icon style (EVToolbox part)
    Change-Id: Id4176e3fec2f37722270b19591582dcfbd3ed776

--- src/com/evervolv/toolbox/activities/subactivities/StatusbarIcons.java
+++ src/com/evervolv/toolbox/activities/subactivities/StatusbarIcons.java
@@ -3,22 +3,26 @@ package com.evervolv.toolbox.activities.subactivities;
 import android.content.ContentResolver;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 
 import com.evervolv.toolbox.R;
 import com.evervolv.toolbox.SettingsFragment;
 
-public class StatusbarIcons extends SettingsFragment {
+public class StatusbarIcons extends SettingsFragment implements OnPreferenceChangeListener {
 
     private static final String STATUSBAR_SIXBAR_SIGNAL = "pref_statusbar_sixbar_signal";
-    
+    private static final String STATUSBAR_BATTERY_STYLE = "pref_statusbar_batt_style";
+
     private ContentResolver mCr;
     private PreferenceScreen mPrefSet;
-    
+
     private CheckBoxPreference mUseSixbaricons;
-    
+    private ListPreference mBattStyle;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,22 +31,37 @@ public class StatusbarIcons extends SettingsFragment {
 
         mPrefSet = getPreferenceScreen();
         mCr = getContentResolver();
-        
-        /* Volume button music controls */
+
+        /* Sixbar Signal Icons */
         mUseSixbaricons = (CheckBoxPreference) mPrefSet.findPreference(
                 STATUSBAR_SIXBAR_SIGNAL);
         mUseSixbaricons.setChecked(Settings.System.getInt(getContentResolver(),
                 Settings.System.STATUSBAR_6BAR_SIGNAL, 1) == 1);
-        
+
+        /* Battery Icon Style */
+        mBattStyle = (ListPreference) mPrefSet.findPreference(STATUSBAR_BATTERY_STYLE);
+        mBattStyle.setOnPreferenceChangeListener(this);
+
     }
-    
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mBattStyle) {
+            Settings.System.putInt(mCr, Settings.System.STATUSBAR_BATT_STYLE,
+                    Integer.valueOf((String) newValue));
+            return true;
+        }
+        return false;
+    }
+
+    @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         boolean value;
         if (preference == mUseSixbaricons) {
             value = mUseSixbaricons.isChecked();
             Settings.System.putInt(mCr, Settings.System.STATUSBAR_6BAR_SIGNAL,
                     value ? 1 : 0);
-            return true;   
+            return true;
         }
         return false;
     }
