152323bc395ae83f2ab04fb2fe4f0803bc999101
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-06-28 16:35:17 -0500

    EVToolbox: add toggle for longpress back
    Change-Id: I6fd534b2adf0a80c6dce8e494eaf219b4d77685b

--- src/com/evervolv/toolbox/activities/subactivities/InterfaceButtons.java
+++ src/com/evervolv/toolbox/activities/subactivities/InterfaceButtons.java
@@ -16,6 +16,7 @@ public class InterfaceButtons extends SettingsFragment {
     private static final String TRACKBALL_WAKE_TOGGLE = "pref_trackball_wake_toggle";
     private static final String VOLUME_WAKE_TOGGLE = "pref_volume_wake_toggle";
     private static final String LOCKSCREEN_MUSIC_CTRL_VOLBTN = "pref_lockscreen_music_controls_volbtn";
+    private static final String KILL_APP_LONGPRESS_BACK = "pref_kill_app_longpress_back";
 
     private ContentResolver mCr;
     private PreferenceScreen mPrefSet;
@@ -23,6 +24,7 @@ public class InterfaceButtons extends SettingsFragment {
     private CheckBoxPreference mTrackballWake;
     private CheckBoxPreference mVolumeWake;
     private CheckBoxPreference mMusicCtrlVolBtn;
+    private CheckBoxPreference mKillAppBackBtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,6 +52,11 @@ public class InterfaceButtons extends SettingsFragment {
         mMusicCtrlVolBtn.setChecked(Settings.System.getInt(mCr,
                 Settings.System.LOCKSCREEN_MUSIC_CONTROLS_VOLBTN, 1) == 1);
 
+        /* Kill app long press pack */
+        mKillAppBackBtn = (CheckBoxPreference) mPrefSet.findPreference(KILL_APP_LONGPRESS_BACK);
+        mKillAppBackBtn.setChecked(Settings.Secure.getInt(mCr,
+                Settings.Secure.KILL_APP_LONGPRESS_BACK, 0) == 1);
+
         /* Remove mTrackballWake on devices without trackballs */
         if (!getResources().getBoolean(R.bool.has_trackball)) {
             mPrefSet.removePreference(mTrackballWake);
@@ -72,6 +79,11 @@ public class InterfaceButtons extends SettingsFragment {
         } else if (preference == mMusicCtrlVolBtn) {
             value = mMusicCtrlVolBtn.isChecked();
             Settings.System.putInt(mCr, Settings.System.LOCKSCREEN_MUSIC_CONTROLS_VOLBTN,
+                    value ? 1 : 0);
+            return true;
+        } else if (preference == mKillAppBackBtn) {
+            value = mKillAppBackBtn.isChecked();
+            Settings.Secure.putInt(mCr, Settings.Secure.KILL_APP_LONGPRESS_BACK,
                     value ? 1 : 0);
             return true;
         }
