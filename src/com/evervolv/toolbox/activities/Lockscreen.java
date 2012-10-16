27922d9d92d81dd37dc172ea9efb0dca99f71c52
andrew.boren <andrew.boren@gmail.com>
2012-10-15 22:36:31 -0700

    EVToolbox: Add transparent lockscreen toggle.
    Change-Id: I888b75dd3152bf5df6de9b91170219142f311d4f

--- src/com/evervolv/toolbox/activities/Lockscreen.java
+++ src/com/evervolv/toolbox/activities/Lockscreen.java
@@ -15,12 +15,14 @@ import com.evervolv.toolbox.activities.subactivities.LockscreenStyle;
 public class Lockscreen extends SettingsFragment {
 
     private static final String LOCKSCREEN_STYLE_PREF = "pref_lockscreen_style";
+    private static final String LOCKSCREEN_TRANSPARENT_PREF = "pref_lockscreen_transparent";
 
     private static final String TAG = "EVToolbox";
 
     private ContentResolver mCr;
     private PreferenceScreen mPrefSet;
     private PreferenceScreen mLockStyle;
+    private CheckBoxPreference mLockTransparent;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,14 +34,21 @@ public class Lockscreen extends SettingsFragment {
         mCr = getContentResolver();
         mLockStyle = (PreferenceScreen) mPrefSet.findPreference(
                 LOCKSCREEN_STYLE_PREF);
+        mLockTransparent = (CheckBoxPreference) mPrefSet.findPreference(
+                LOCKSCREEN_TRANSPARENT_PREF);
     }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        boolean value;
         if (preference == mLockStyle) {
             startPreferencePanel(mLockStyle.getFragment(),
                     null, mLockStyle.getTitleRes(), null, null, -1);
             return true;
+        } else if (preference == mLockTransparent) {
+            value = mLockTransparent.isChecked();
+            Settings.System.putInt(mCr, Settings.System.LOCKSCREEN_TRANSPARENT,
+                    value ? 1 : 0);
         }
         return false;
     }
