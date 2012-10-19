3b4eefd3b9f998aeb98a259ea7e8830e0faecb43
andrew.boren <andrew.boren@gmail.com>
2012-10-18 20:30:44 -0700

    EVToolbox: Add toggle for sense 4.0 recent apps view.
    Change-Id: I82ecd0b729306d1342d462afe833760543774e8a

--- src/com/evervolv/toolbox/activities/Interface.java
+++ src/com/evervolv/toolbox/activities/Interface.java
@@ -1,8 +1,11 @@
 package com.evervolv.toolbox.activities;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
+import android.provider.Settings;
 import android.util.Log;
 
 import com.evervolv.toolbox.R;
@@ -18,11 +21,14 @@ public class Interface extends SettingsFragment {
     private static final String BUTTONS_PREF = "pref_interface_buttons";
     private static final String ROTATION_PREF = "pref_interface_rotation";
     private static final String POWER_MENU_PREF = "pref_interface_power_menu";
+    private static final String SENSE4_RECENT_APPS_PREF = "pref_interface_sense4_recent_apps";
 
+    private ContentResolver mCr;
     private PreferenceScreen mPrefSet;
     private PreferenceScreen mButtons;
     private PreferenceScreen mRotation;
     private PreferenceScreen mPowerMenu;
+    private CheckBoxPreference mSense4RecentApps;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,14 +36,17 @@ public class Interface extends SettingsFragment {
         addPreferencesFromResource(R.xml.interface_settings);
 
         mPrefSet = getPreferenceScreen();
+        mCr = getContentResolver();
 
         mButtons = (PreferenceScreen) mPrefSet.findPreference(BUTTONS_PREF);
         mRotation = (PreferenceScreen) mPrefSet.findPreference(ROTATION_PREF);
         mPowerMenu = (PreferenceScreen) mPrefSet.findPreference(POWER_MENU_PREF);
+        mSense4RecentApps = (CheckBoxPreference) mPrefSet.findPreference(SENSE4_RECENT_APPS_PREF);
     }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        boolean value;
         if (preference == mButtons) {
             startPreferencePanel(mButtons.getFragment(),
                     null, mButtons.getTitleRes(), null, null, -1);
@@ -49,6 +58,11 @@ public class Interface extends SettingsFragment {
         } else if (preference == mPowerMenu) {
             startPreferencePanel(mPowerMenu.getFragment(),
                     null, mPowerMenu.getTitleRes(), null, null, -1);
+            return true;
+        } else if (preference == mSense4RecentApps) {
+            value = mSense4RecentApps.isChecked();
+            Settings.System.putInt(mCr, Settings.System.SENSE4_RECENT_APPS,
+                    value ? 1 : 0);
             return true;
         }
         return false;
