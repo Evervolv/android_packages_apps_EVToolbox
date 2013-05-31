7703ecf5b7ee216a9186beb6bc7068eea7787a09
andrew.boren <andrew.boren@gmail.com>
2013-05-30 22:34:06 -0700

    EVToolbox: Add support for a custom lockscreen message
    Change-Id: I98e9778b07a2e463d856e19d1c81eb2e83341f16

--- src/com/evervolv/toolbox/activities/Lockscreen.java
+++ src/com/evervolv/toolbox/activities/Lockscreen.java
@@ -3,8 +3,10 @@ package com.evervolv.toolbox.activities;
 import android.content.ContentResolver;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 import android.util.Log;
 
@@ -12,16 +14,18 @@ import com.evervolv.toolbox.R;
 import com.evervolv.toolbox.SettingsFragment;
 import com.evervolv.toolbox.activities.subactivities.LockscreenStyle;
 
-public class Lockscreen extends SettingsFragment {
+public class Lockscreen extends SettingsFragment implements OnPreferenceChangeListener {
 
     private static final String LOCKSCREEN_STYLE_PREF = "pref_lockscreen_style";
     private static final String LOCKSCREEN_TRANSPARENT_PREF = "pref_lockscreen_transparent";
+    private static final String LOCKSCREEN_MESSAGE_PREF = "pref_lockscreen_message";
 
     private static final String TAG = "EVToolbox";
 
     private ContentResolver mCr;
     private PreferenceScreen mPrefSet;
     private PreferenceScreen mLockStyle;
+    private EditTextPreference mLockMessage;
     private CheckBoxPreference mLockTransparent;
 
     @Override
@@ -34,10 +38,29 @@ public class Lockscreen extends SettingsFragment {
         mCr = getContentResolver();
         mLockStyle = (PreferenceScreen) mPrefSet.findPreference(
                 LOCKSCREEN_STYLE_PREF);
+
+        mLockMessage = (EditTextPreference) mPrefSet.findPreference(
+                LOCKSCREEN_MESSAGE_PREF);
+        mLockMessage.setOnPreferenceChangeListener(this);
+        String message = Settings.System.getString(mCr,
+                Settings.System.LOCKSCREEN_MESSAGE);
+        setLockMessageSummaryAndText(message);
+
         mLockTransparent = (CheckBoxPreference) mPrefSet.findPreference(
                 LOCKSCREEN_TRANSPARENT_PREF);
         // unimplemented yet
         mPrefSet.removePreference(mLockTransparent);
+    }
+
+    private void setLockMessageSummaryAndText(String message) {
+        if (message == null || message.equals("")) {
+            mLockMessage.setText("");
+            mLockMessage.setSummary(R.string
+                    .pref_lockscreen_message_summary);
+        } else {
+            mLockMessage.setSummary(message);
+            mLockMessage.setText(message);
+        }
     }
 
     @Override
@@ -52,6 +75,22 @@ public class Lockscreen extends SettingsFragment {
             //value = mLockTransparent.isChecked();
             //Settings.System.putInt(mCr, Settings.System.LOCKSCREEN_TRANSPARENT,
             //        value ? 1 : 0);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLockMessage) {
+            String value = newValue.toString();
+            setLockMessageSummaryAndText(value);
+            if (value.equals("")) {
+                Settings.System.putString(mCr, Settings.System.LOCKSCREEN_MESSAGE,
+                        null);
+            } else {
+                Settings.System.putString(mCr, Settings.System.LOCKSCREEN_MESSAGE,
+                        value);
+            }
         }
         return false;
     }
