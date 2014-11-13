f716dbb725cda18f5bd2a3df6c0870d935e48acf
Adnan <adnan@cyngn.com>
2014-11-12 17:14:27 -0800

    Settings : Hardware key rebinding (2/2)
    Change-Id: I1a0546f358e787a9c6ab12024e34255475259424

---
+++ src/com/evervolv/toolbox/input/ButtonSettings.java
@@ -0,0 +1,243 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.evervolv.toolbox.input;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceGroup;
+import android.support.v7.preference.PreferenceScreen;
+import android.util.Log;
+
+import evervolv.provider.EVSettings;
+import com.evervolv.toolbox.R;
+import com.evervolv.toolbox.SettingsPreferenceFragment;
+
+import java.util.List;
+
+public class ButtonSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "ButtonSettings";
+
+    private static final String KEY_HOME_LONG_PRESS = "button_home_long_press";
+    private static final String KEY_HOME_DOUBLE_TAP = "button_home_double_tap";
+    private static final String KEY_MENU_PRESS = "button_menu_press";
+    private static final String KEY_MENU_LONG_PRESS = "button_menu_long_press";
+    private static final String KEY_ASSIST_PRESS = "button_assist_press";
+    private static final String KEY_ASSIST_LONG_PRESS = "button_assist_long_press";
+    private static final String KEY_APP_SWITCH_PRESS = "button_app_switch_press";
+    private static final String KEY_APP_SWITCH_LONG_PRESS = "button_app_switch_long_press";
+
+    private static final String CATEGORY_HOME = "home_key";
+    private static final String CATEGORY_BACK = "back_key";
+    private static final String CATEGORY_MENU = "menu_key";
+    private static final String CATEGORY_ASSIST = "assist_key";
+    private static final String CATEGORY_APPSWITCH = "app_switch_key";
+
+    // Available custom actions to perform on a key press.
+    // Must match values for KEY_HOME_LONG_PRESS_ACTION in:
+    // vendor/ev/sdk/core/java/evervolv/provider/EVSettings.java
+    private enum Action {
+        NOTHING,
+        MENU,
+        APP_SWITCH,
+        SEARCH,
+        VOICE_SEARCH,
+        IN_APP_SEARCH,
+        LAUNCH_CAMERA,
+        SLEEP,
+        LAST_APP,
+        SPLIT_SCREEN;
+
+        public static Action fromIntSafe(int id) {
+            if (id < NOTHING.ordinal() || id > Action.values().length) {
+                return NOTHING;
+            }
+            return Action.values()[id];
+        }
+
+        public static Action fromSettings(ContentResolver cr, String setting, Action def) {
+            return fromIntSafe(EVSettings.System.getInt(cr, setting, def.ordinal()));
+        }
+    }
+
+    // Masks for checking presence of hardware keys.
+    // Must match values in frameworks/base/core/res/res/values/config.xml
+    public static final int KEY_MASK_HOME = 0x01;
+    public static final int KEY_MASK_BACK = 0x02;
+    public static final int KEY_MASK_MENU = 0x04;
+    public static final int KEY_MASK_ASSIST = 0x08;
+    public static final int KEY_MASK_APP_SWITCH = 0x10;
+    public static final int KEY_MASK_CAMERA = 0x20;
+    public static final int KEY_MASK_VOLUME = 0x40;
+
+    private ListPreference mHomeLongPressAction;
+    private ListPreference mHomeDoubleTapAction;
+    private ListPreference mMenuPressAction;
+    private ListPreference mMenuLongPressAction;
+    private ListPreference mAssistPressAction;
+    private ListPreference mAssistLongPressAction;
+    private ListPreference mAppSwitchPressAction;
+    private ListPreference mAppSwitchLongPressAction;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.button_settings);
+
+        final Resources res = getResources();
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        final int deviceKeys = res.getInteger(
+                com.evervolv.platform.internal.R.integer.config_deviceHardwareKeys);
+
+        final boolean hasHomeKey = (deviceKeys & KEY_MASK_HOME) != 0;
+        final boolean hasMenuKey = (deviceKeys & KEY_MASK_MENU) != 0;
+        final boolean hasAssistKey = (deviceKeys & KEY_MASK_ASSIST) != 0;
+        final boolean hasAppSwitchKey = (deviceKeys & KEY_MASK_APP_SWITCH) != 0;
+
+        final PreferenceGroup homeCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_HOME);
+        final PreferenceGroup menuCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_MENU);
+        final PreferenceGroup assistCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_ASSIST);
+        final PreferenceGroup appSwitchCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_APPSWITCH);
+
+        Action defaultHomeLongPressAction = Action.fromIntSafe(res.getInteger(
+                com.android.internal.R.integer.config_longPressOnHomeBehavior));
+        Action defaultHomeDoubleTapAction = Action.fromIntSafe(res.getInteger(
+                com.android.internal.R.integer.config_doubleTapOnHomeBehavior));
+        Action homeLongPressAction = Action.fromSettings(resolver,
+                EVSettings.System.KEY_HOME_LONG_PRESS_ACTION,
+                defaultHomeLongPressAction);
+        Action homeDoubleTapAction = Action.fromSettings(resolver,
+                EVSettings.System.KEY_HOME_DOUBLE_TAP_ACTION,
+                defaultHomeDoubleTapAction);
+
+        if (hasHomeKey) {
+            mHomeLongPressAction = initList(KEY_HOME_LONG_PRESS, homeLongPressAction);
+            mHomeDoubleTapAction = initList(KEY_HOME_DOUBLE_TAP, homeDoubleTapAction);
+        } else {
+            prefScreen.removePreference(homeCategory);
+        }
+
+        if (hasMenuKey) {
+            Action pressAction = Action.fromSettings(resolver,
+                    EVSettings.System.KEY_MENU_ACTION, Action.MENU);
+            mMenuPressAction = initList(KEY_MENU_PRESS, pressAction);
+
+            Action longPressAction = Action.fromSettings(resolver,
+                        EVSettings.System.KEY_MENU_LONG_PRESS_ACTION,
+                        hasAssistKey ? Action.NOTHING : Action.SEARCH);
+            mMenuLongPressAction = initList(KEY_MENU_LONG_PRESS, longPressAction);
+        } else {
+            prefScreen.removePreference(menuCategory);
+        }
+
+        if (hasAssistKey) {
+            Action pressAction = Action.fromSettings(resolver,
+                    EVSettings.System.KEY_ASSIST_ACTION, Action.SEARCH);
+            mAssistPressAction = initList(KEY_ASSIST_PRESS, pressAction);
+
+            Action longPressAction = Action.fromSettings(resolver,
+                    EVSettings.System.KEY_ASSIST_LONG_PRESS_ACTION, Action.VOICE_SEARCH);
+            mAssistLongPressAction = initList(KEY_ASSIST_LONG_PRESS, longPressAction);
+        } else {
+            prefScreen.removePreference(assistCategory);
+        }
+
+        if (hasAppSwitchKey) {
+            Action pressAction = Action.fromSettings(resolver,
+                    EVSettings.System.KEY_APP_SWITCH_ACTION, Action.APP_SWITCH);
+            mAppSwitchPressAction = initList(KEY_APP_SWITCH_PRESS, pressAction);
+
+            Action longPressAction = Action.fromSettings(resolver,
+                    EVSettings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION, Action.SPLIT_SCREEN);
+            mAppSwitchLongPressAction = initList(KEY_APP_SWITCH_LONG_PRESS, longPressAction);
+        } else {
+            prefScreen.removePreference(appSwitchCategory);
+        }
+    }
+
+    private ListPreference initList(String key, Action value) {
+        return initList(key, value.ordinal());
+    }
+
+    private ListPreference initList(String key, int value) {
+        ListPreference list = (ListPreference) getPreferenceScreen().findPreference(key);
+        if (list == null) return null;
+        list.setValue(Integer.toString(value));
+        list.setSummary(list.getEntry());
+        list.setOnPreferenceChangeListener(this);
+        return list;
+    }
+
+    private void handleListChange(ListPreference pref, Object newValue, String setting) {
+        String value = (String) newValue;
+        int index = pref.findIndexOfValue(value);
+        pref.setSummary(pref.getEntries()[index]);
+        EVSettings.System.putInt(getContentResolver(), setting, Integer.valueOf(value));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mHomeLongPressAction) {
+            handleListChange((ListPreference) preference, newValue,
+                    EVSettings.System.KEY_HOME_LONG_PRESS_ACTION);
+            return true;
+        } else if (preference == mHomeDoubleTapAction) {
+            handleListChange((ListPreference) preference, newValue,
+                    EVSettings.System.KEY_HOME_DOUBLE_TAP_ACTION);
+            return true;
+        } else if (preference == mMenuPressAction) {
+            handleListChange(mMenuPressAction, newValue,
+                    EVSettings.System.KEY_MENU_ACTION);
+            return true;
+        } else if (preference == mMenuLongPressAction) {
+            handleListChange(mMenuLongPressAction, newValue,
+                    EVSettings.System.KEY_MENU_LONG_PRESS_ACTION);
+            return true;
+        } else if (preference == mAssistPressAction) {
+            handleListChange(mAssistPressAction, newValue,
+                    EVSettings.System.KEY_ASSIST_ACTION);
+            return true;
+        } else if (preference == mAssistLongPressAction) {
+            handleListChange(mAssistLongPressAction, newValue,
+                    EVSettings.System.KEY_ASSIST_LONG_PRESS_ACTION);
+            return true;
+        } else if (preference == mAppSwitchPressAction) {
+            handleListChange(mAppSwitchPressAction, newValue,
+                    EVSettings.System.KEY_APP_SWITCH_ACTION);
+            return true;
+        } else if (preference == mAppSwitchLongPressAction) {
+            handleListChange(mAppSwitchLongPressAction, newValue,
+                    EVSettings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION);
+            return true;
+        }
+        return false;
+    }
+}
