3b0003ffeea240724c37f4db66a2124ea2c566f8
Nick Reuter <nreuter85@gmail.com>
2015-10-19 02:19:30 -0500

    EVToolbox: include legacy apache
    Change-Id: Ic9004a3a3d78ac1a4acf811ec50ba43a3b777523

--- Android.mk
+++ Android.mk
@@ -9,7 +9,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_JAVA_LIBRARIES += \
     android-support-v13 \
     android-support-v4 \
-    android-support-v7-appcompat
+    android-support-v7-appcompat \
+    org.apache.http.legacy
 
 LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dir))
 LOCAL_SRC_FILES := $(call all-java-files-under, src/)
