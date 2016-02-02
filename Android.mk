afea0bf2d50005ed7d4b2e23276d3dab8bee0daf
Nick Reuter <nreuter85@gmail.com>
2016-02-02 17:04:16 -0600

    Toolbox: cleanup makefile and manifest
    Change-Id: Ia2a7e6a87427a8f0e9a2ae6e05ed65883987eb15

--- Android.mk
+++ Android.mk
@@ -1,16 +1,10 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-appcompat_dir := ../../../prebuilts/sdk/current/support/v7/appcompat/res
-res_dir := res $(appcompat_dir)
-
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES += \
-    android-support-v13 \
-    android-support-v4 \
-    android-support-v7-appcompat \
-    org.apache.http.legacy
+appcompat_dir := ../../../$(SUPPORT_LIBRARY_ROOT)/v7/appcompat/res
+res_dir := res $(appcompat_dir)
 
 LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dir))
 LOCAL_SRC_FILES := $(call all-java-files-under, src/)
@@ -20,12 +14,18 @@ ifneq ($(BOARD_DEVICE_SETTINGS),)
     LOCAL_SRC_FILES += $(call all-java-files-under, ../../../$(BOARD_DEVICE_SETTINGS)/src)
 endif
 
-LOCAL_AAPT_FLAGS := --auto-add-overlay
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v13 \
+    android-support-v7-appcompat \
+    android-support-v4 \
+    org.apache.http.legacy
+
+LOCAL_AAPT_FLAGS := \
+    --auto-add-overlay \
+    --extra-packages android.support.v7.appcompat
 
 LOCAL_PACKAGE_NAME := EVToolbox
 LOCAL_CERTIFICATE := platform
-
 LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
