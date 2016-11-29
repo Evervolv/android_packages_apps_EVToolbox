4bcf20df377b8d18c2775f64cf67969e8c0bf633
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2016-11-29 08:03:52 -0500

    EVToolbox: update battery options
    Battery icon: let disable force text outside icon when charging [2/2]
    Change-Id: I531aa7f1a10b29691cd2171a82445b85eb4b3bc3
    
    Battery: allow set a custom symbol near text pct on charging [2/2]
    Change-Id: Ibc7aca0295f5089995e5cc8b98c71751f73775b1

--- src/com/evervolv/toolbox/fragments/StatusbarGeneral.java
+++ src/com/evervolv/toolbox/fragments/StatusbarGeneral.java
@@ -40,6 +40,8 @@ public class StatusbarGeneral extends PreferenceFragment implements
     private static final String STATUSBAR_BATTERY_PERCENT = "pref_statusbar_batt_percent_style";
     private static final String STATUSBAR_BATTERY_STYLE_TILE = "pref_status_bar_battery_style_tile";
     private static final String STATUSBAR_CHARGE_COLOR = "pref_status_bar_charge_color";
+    private static final String STATUSBAR_BATTERY_CHARGE_TEXT = "pref_status_bar_battery_charge_text";
+    private static final String STATUSBAR_BATTERY_TEXT_SYMBOL = "pref_status_bar_battery_text_symbol";
     private static final String STATUSBAR_CLOCK_AM_PM_STYLE = "pref_statusbar_clock_am_pm_style";
     private static final String STATUSBAR_QUICK_PULLDOWN = "pref_statusbar_quick_pulldown";
 
@@ -53,9 +55,15 @@ public class StatusbarGeneral extends PreferenceFragment implements
     private ColorPickerPreference mChargeColor;
     private SwitchPreference mQuickPulldown;
     private SwitchPreference mQsBatteryTitle;
+    private SwitchPreference mForceChargeText;
     private ListPreference mBattStyle;
     private ListPreference mBattPercent;
     private ListPreference mClockAmPmStyle;
+    private ListPreference mTextChargingSymbol;
+
+    private int mBattStyleValue;
+    private int mBattPercentValue;
+    private int mTextChargingSymbolValue;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -68,14 +76,16 @@ public class StatusbarGeneral extends PreferenceFragment implements
 
         /* Battery Icon Style */
         mBattStyle = (ListPreference) mPrefSet.findPreference(STATUSBAR_BATTERY_STYLE);
-        mBattStyle.setValue(Integer.toString(Settings.System.getInt(mCr,
-                Settings.Secure.STATUS_BAR_BATTERY_STYLE, BATT_STYLE_DEFAULT)));
+        mBattStyleValue = Settings.Secure.getInt(mCr,
+                Settings.Secure.STATUS_BAR_BATTERY_STYLE, BATT_STYLE_DEFAULT);
+        mBattStyle.setValue(Integer.toString(mBattStyleValue));
         mBattStyle.setOnPreferenceChangeListener(this);
 
         /* Battery Percent */
         mBattPercent = (ListPreference) mPrefSet.findPreference(STATUSBAR_BATTERY_PERCENT);
-        mBattPercent.setValue(Integer.toString(Settings.System.getInt(mCr,
-                Settings.Secure.STATUS_BAR_SHOW_BATTERY_PERCENT, BATT_PERCENT_DEFAULT)));
+        mBattPercentValue = Settings.Secure.getInt(mCr,
+                Settings.Secure.STATUS_BAR_SHOW_BATTERY_PERCENT, BATT_PERCENT_DEFAULT);
+        mBattPercent.setValue(Integer.toString(mBattPercentValue));
         mBattPercent.setOnPreferenceChangeListener(this);
 
         mQsBatteryTitle = (SwitchPreference) findPreference(STATUSBAR_BATTERY_STYLE_TILE);
@@ -87,6 +97,17 @@ public class StatusbarGeneral extends PreferenceFragment implements
                 Settings.Secure.STATUS_BAR_CHARGE_COLOR, Color.WHITE));
         mChargeColor.setOnPreferenceChangeListener(this);
 
+        mForceChargeText = (SwitchPreference) findPreference(STATUSBAR_BATTERY_CHARGE_TEXT);
+        mForceChargeText.setChecked((Settings.Secure.getInt(mCr,
+                Settings.Secure.FORCE_CHARGE_BATTERY_TEXT, 1) == 1));
+
+        mTextChargingSymbol = (ListPreference) findPreference(STATUSBAR_BATTERY_TEXT_SYMBOL);
+        mTextChargingSymbolValue = Settings.Secure.getInt(mCr,
+                Settings.Secure.TEXT_CHARGING_SYMBOL, 0);
+        mTextChargingSymbol.setValue(Integer.toString(mTextChargingSymbolValue));
+        mTextChargingSymbol.setSummary(mTextChargingSymbol.getEntry());
+        mTextChargingSymbol.setOnPreferenceChangeListener(this);
+
         /* Clock AM/PM Style */
         mClockAmPmStyle = (ListPreference) mPrefSet.findPreference(STATUSBAR_CLOCK_AM_PM_STYLE);
         mClockAmPmStyle.setValue(Integer.toString(Settings.System.getInt(mCr,
@@ -97,6 +118,8 @@ public class StatusbarGeneral extends PreferenceFragment implements
         mQuickPulldown = (SwitchPreference) mPrefSet.findPreference(STATUSBAR_QUICK_PULLDOWN);
         mQuickPulldown.setChecked(Settings.System.getInt(mCr,
                 Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 1) == 1);
+
+        enableStatusBarBatteryDependents();
     }
 
     @Override
@@ -115,13 +138,17 @@ public class StatusbarGeneral extends PreferenceFragment implements
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == mBattStyle) {
+            mBattStyleValue = Integer.valueOf((String) newValue);
             Settings.Secure.putInt(mCr, Settings.Secure.STATUS_BAR_BATTERY_STYLE,
-                    Integer.valueOf((String) newValue));
+                    mBattStyleValue);
+            enableStatusBarBatteryDependents();
             return true;
         }
         if (preference == mBattPercent) {
+            mBattPercentValue = Integer.valueOf((String) newValue);
             Settings.Secure.putInt(mCr, Settings.Secure.STATUS_BAR_SHOW_BATTERY_PERCENT,
-                    Integer.valueOf((String) newValue));
+                    mBattPercentValue);
+            enableStatusBarBatteryDependents();
             return true;
         }
         if (preference == mClockAmPmStyle) {
@@ -132,6 +159,13 @@ public class StatusbarGeneral extends PreferenceFragment implements
         if (preference.equals(mChargeColor)) {
             Settings.Secure.putInt(mCr, Settings.Secure.STATUS_BAR_CHARGE_COLOR,
                     ((Integer) newValue).intValue());
+            enableStatusBarBatteryDependents();
+            return true;
+        }
+        if (preference == mTextChargingSymbol) {
+            mTextChargingSymbolValue = Integer.valueOf((String) newValue);
+            Settings.Secure.putInt(mCr, Settings.Secure.TEXT_CHARGING_SYMBOL,
+                    mTextChargingSymbolValue);
             return true;
         }
         return false;
@@ -151,7 +185,46 @@ public class StatusbarGeneral extends PreferenceFragment implements
             Settings.Secure.putInt(mCr, Settings.Secure.STATUS_BAR_BATTERY_STYLE_TILE,
                     value ? 1 : 0);
         }
+        if (preference == mForceChargeText) {
+            value = mForceChargeText.isChecked();
+            Settings.Secure.putInt(mCr, Settings.Secure.FORCE_CHARGE_BATTERY_TEXT,
+                    value ? 1 : 0);
+            return true;
+        }
         return false;
+    }
+
+    private void enableStatusBarBatteryDependents() {
+        switch (mBattStyleValue) {
+        case 0: // PORTRAIT
+            mBattPercent.setEnabled(true);
+            mQsBatteryTitle.setEnabled(false);
+            mChargeColor.setEnabled(true);
+            mForceChargeText.setEnabled(mBattPercentValue == 2 ? false : true);
+            mTextChargingSymbol.setEnabled(true);
+            break;
+        case 4: // HIDDEN
+            mBattPercent.setEnabled(false);
+            mQsBatteryTitle.setEnabled(false);
+            mForceChargeText.setEnabled(false);
+            mChargeColor.setEnabled(false);
+            mTextChargingSymbol.setEnabled(false);
+            break;
+        case 6: // TEXT
+            mBattPercent.setEnabled(false);
+            mQsBatteryTitle.setEnabled(false);
+            mForceChargeText.setEnabled(false);
+            mChargeColor.setEnabled(false);
+            mTextChargingSymbol.setEnabled(true);
+            break;
+        default:
+            mBattPercent.setEnabled(true);
+            mQsBatteryTitle.setEnabled(true);
+            mChargeColor.setEnabled(true);
+            mForceChargeText.setEnabled(mBattPercentValue == 2 ? false : true);
+            mTextChargingSymbol.setEnabled(true);
+            break;
+        }
     }
 
     @Override
