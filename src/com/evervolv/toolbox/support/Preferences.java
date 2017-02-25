d231f9ead87cb22f1327a77dc73a1fe26c7534f7
Nick Reuter <nreuter85@gmail.com>
2017-02-24 18:10:12 -0600

    Toolbox: update copyright year
    Change-Id: I3c7e207db46878c3505e40ebb6b6f4e92ed5e718

--- src/com/evervolv/toolbox/support/Preferences.java
+++ src/com/evervolv/toolbox/support/Preferences.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 The Evervolv Project
+ * Copyright (C) 2013-2017 The Evervolv Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
