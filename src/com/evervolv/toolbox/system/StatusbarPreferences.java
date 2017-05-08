dad87c3a97f7245f2247bbafcb31c6c1ab22b2c9
ezio84 <brabus84@gmail.com>
2017-05-08 17:35:11 +0200

    Toolbox: Battery icon: make it more theme friendly [2/2]
    Change-Id: I2bc685a3cd108ba48de469d7174e1272effba3ae

--- src/com/evervolv/toolbox/system/StatusbarPreferences.java
+++ src/com/evervolv/toolbox/system/StatusbarPreferences.java
@@ -16,44 +16,15 @@
 
 package com.evervolv.toolbox.system;
 
-import android.content.ContentResolver;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 
 import com.evervolv.toolbox.R;
-import com.evervolv.toolbox.Toolbox;
 import com.evervolv.toolbox.ToolboxPreferenceFragment;
 
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
-
-public class StatusbarPreferences extends ToolboxPreferenceFragment implements
-        OnPreferenceChangeListener, Toolbox.DisabledListener {
-
-    private static final String PREF_BATTERY_ICON_COLOR = "pref_battery_icon_color";
-
-    private ColorPickerPreference mBatteryIconColor;
+public class StatusbarPreferences extends ToolboxPreferenceFragment {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.statusbar);
-
-        mBatteryIconColor = (ColorPickerPreference) findPreference(PREF_BATTERY_ICON_COLOR);
-        mBatteryIconColor.setNewPreviewColor(Settings.Secure.getInt(
-                getActivity().getContentResolver(),
-                Settings.Secure.STATUS_BAR_CHARGE_COLOR, Color.WHITE));
-        mBatteryIconColor.setOnPreferenceChangeListener(this);
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mBatteryIconColor) {
-            Settings.Secure.putInt(getActivity().getContentResolver(),
-                    Settings.Secure.STATUS_BAR_CHARGE_COLOR, ((Integer) newValue).intValue());
-            return true;
-        }
-        return false;
     }
 }
