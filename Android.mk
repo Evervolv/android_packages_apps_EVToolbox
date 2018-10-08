500db8b26a4a28f6bfe58df76425167cb6297103
Nick Reuter <nreuter85@gmail.com>
2018-10-07 23:19:43 -0500

    Toolbox: start fresh with parts support
    Change-Id: I68b1d67444fb76457aca7043fcfe05912b83a838

--- Android.mk
+++ Android.mk
@@ -1,43 +1,43 @@
 LOCAL_PATH:= $(call my-dir)
+
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional
+LOCAL_PACKAGE_NAME := EVToolbox
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_RESOURCE_DIR := \
-    $(LOCAL_PATH)/res \
-    frameworks/support/v7/preference/res \
-    frameworks/support/v14/preference/res \
-    frameworks/support/v7/appcompat/res \
-    frameworks/support/v7/recyclerview/res \
-    frameworks/support/design/res \
-    vendor/cm/support/preference/res
-
-LOCAL_SRC_FILES := $(call all-java-files-under, src/)
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    android-support-dynamic-animation \
+    android-support-v4 \
+    android-support-v7-appcompat \
+    android-support-v7-palette \
+    android-support-v7-preference \
+    android-support-v7-recyclerview \
+    android-support-v13 \
+    android-support-v14-preference
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-design \
-    android-support-v4 \
-    android-support-v7-recyclerview \
-    android-support-v7-preference \
-    android-support-v7-appcompat \
-    android-support-v13 \
-    android-support-v14-preference \
-    org.apache.http.legacy \
-    cm-support-preference
+    jsr305 \
+    com.evervolv.platform.internal
 
-LOCAL_AAPT_FLAGS := \
-    --auto-add-overlay \
-    --extra-packages android.support.v7.recyclerview \
-    --extra-packages android.support.v7.preference \
-    --extra-packages android.support.v14.preference \
-    --extra-packages android.support.v7.appcompat \
-    --extra-packages android.support.design \
-    --extra-packages org.cyanogenmod.support.preference
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res
 
-LOCAL_PACKAGE_NAME := EVToolbox
+LOCAL_USE_AAPT2 := true
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
-
+LOCAL_MODULE_TAGS := optional
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+ifneq ($(INCREMENTAL_BUILDS),)
+    LOCAL_PROGUARD_ENABLED := disabled
+    LOCAL_JACK_ENABLED := incremental
+    LOCAL_DX_FLAGS := --multi-dex
+    LOCAL_JACK_FLAGS := --multi-dex native
+endif
+
+include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
