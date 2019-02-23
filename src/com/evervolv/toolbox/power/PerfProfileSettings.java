LineageOS/android_packages_apps_CMParts

cmparts: Refactor for RemotePreference and SettingsHelper
 * Use the new stuff in the SDK to standardize on how to deal with
   remote components and settings.
 * Eliminates code and should be more secure.

Change-Id: I9fd148a844877004cd1526a5fc86d3d1529ecbc8

src/org/cyanogenmod/cmparts/power/PerfProfileSettings.java
@@ -113,7 +113,7 @@ public class PerfProfileSettings extends SettingsPreferenceFragment
             mPerfSeekBar.setOnPreferenceChangeListener(this);
             updatePerfSettings();
 
-            addTrigger(CMSettings.Secure.getUriFor(CMSettings.Secure.PERFORMANCE_PROFILE));
+            watch(CMSettings.Secure.getUriFor(CMSettings.Secure.PERFORMANCE_PROFILE));
         }
 
         mAutoPowerSavePref.setEntries(R.array.auto_power_save_entries);
@@ -246,8 +246,8 @@ public class PerfProfileSettings extends SettingsPreferenceFragment
     }
 
     @Override
-    public void onRefresh(Context context, Uri uri) {
-        super.onRefresh(context, uri);
+    public void onSettingsChanged(Uri contentUri) {
+        super.onSettingsChanged(contentUri);
         updatePerfSettings();
     }
 

Evervolv/android_packages_apps_EVToolbox

src/com/evervolv/toolbox/power/PerfProfileSettings.java
@@ -114,7 +114,7 @@ public class PerfProfileSettings extends SettingsPreferenceFragment
             mPerfSeekBar.setOnPreferenceChangeListener(this);
             updatePerfSettings();
 
-            addTrigger(EVSettings.Secure.getUriFor(EVSettings.Secure.PERFORMANCE_PROFILE));
+            watch(EVSettings.Secure.getUriFor(EVSettings.Secure.PERFORMANCE_PROFILE));
         }
 
         mAutoPowerSavePref.setEntries(R.array.auto_power_save_entries);
@@ -247,8 +247,8 @@ public class PerfProfileSettings extends SettingsPreferenceFragment
     }
 
     @Override
-    public void onRefresh(Context context, Uri uri) {
-        super.onRefresh(context, uri);
+    public void onSettingsChanged(Uri contentUri) {
+        super.onSettingsChanged(contentUri);
         updatePerfSettings();
     }
 