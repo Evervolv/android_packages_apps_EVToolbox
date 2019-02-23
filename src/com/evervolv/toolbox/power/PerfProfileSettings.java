LineageOS/android_packages_apps_CMParts

PerfProfileSettings: Update slider on powersave mode change
The system preference is now updated only when the user changes the
profile, which means that the automatic profile change due to the
powersave mode is not detected by watching the preference. Manually
update the slider when the powersave mode status changes.

Change-Id: Ie1eff7f09e8bf0e26ec9f6ff0f3a6f311ec5ca2e

src/org/cyanogenmod/cmparts/power/PerfProfileSettings.java
@@ -35,6 +35,7 @@ import android.support.v7.preference.Preference;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Toast;
 
+import org.cyanogenmod.cmparts.PartsUpdater;
 import org.cyanogenmod.cmparts.R;
 import org.cyanogenmod.cmparts.widget.SeekBarPreference;
 import org.cyanogenmod.cmparts.SettingsPreferenceFragment;
@@ -46,6 +47,8 @@ import java.util.List;
 import cyanogenmod.power.PerformanceManager;
 import cyanogenmod.power.PerformanceProfile;
 import cyanogenmod.providers.CMSettings;
+
+import static cyanogenmod.power.PerformanceManager.PROFILE_POWER_SAVE;
 
 public class PerfProfileSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
@@ -181,7 +184,8 @@ public class PerfProfileSettings extends SettingsPreferenceFragment
             return;
         }
 
-        PerformanceProfile profile = mPerf.getActivePowerProfile();
+        PerformanceProfile profile = mPowerManager.isPowerSaveMode() ?
+                mPerf.getPowerProfile(PROFILE_POWER_SAVE) : mPerf.getActivePowerProfile();
         mPerfSeekBar.setProgress(mProfiles.indexOf(profile));
         mPerfSeekBar.setTitle(getResources().getString(
                 R.string.perf_profile_title, profile.getName()));
@@ -253,6 +257,9 @@ public class PerfProfileSettings extends SettingsPreferenceFragment
 
     private void updatePowerSaveValue() {
         mPowerSavePref.setChecked(mPowerManager.isPowerSaveMode());
+        updatePerfSettings();
+        // The profile was changed automatically without updating the preference
+        PartsUpdater.notifyChanged(getActivity(), getPreferenceScreen().getKey());
     }
 
     private void updateAutoPowerSaveValue() {
@@ -271,8 +278,12 @@ public class PerfProfileSettings extends SettingsPreferenceFragment
     public static final SummaryProvider SUMMARY_PROVIDER = new SummaryProvider() {
         @Override
         public String getSummary(Context context, String key) {
-            final PerformanceProfile profile =
-                    PerformanceManager.getInstance(context).getActivePowerProfile();
+            final PowerManager powerManager =
+                    (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            final PerformanceManager perfManager = PerformanceManager.getInstance(context);
+            final PerformanceProfile profile = powerManager.isPowerSaveMode() ?
+                    perfManager.getPowerProfile(PROFILE_POWER_SAVE) :
+                    perfManager.getActivePowerProfile();
             String summary = context.getString(R.string.perf_profile_settings_summary);
             if (profile != null) {
                 summary += "\n\n" + context.getResources().getString(

Evervolv/android_packages_apps_EVToolbox

src/com/evervolv/toolbox/power/PerfProfileSettings.java
@@ -37,6 +37,7 @@ import android.widget.Toast;
 
 import evervolv.graphics.drawable.StopMotionVectorDrawable;
 
+import com.evervolv.toolbox.PartsUpdater;
 import com.evervolv.toolbox.R;
 import com.evervolv.toolbox.widget.SeekBarPreference;
 import com.evervolv.toolbox.SettingsPreferenceFragment;
@@ -47,6 +48,8 @@ import java.util.List;
 import evervolv.power.PerformanceManager;
 import evervolv.power.PerformanceProfile;
 import evervolv.provider.EVSettings;
+
+import static evervolv.power.PerformanceManager.PROFILE_POWER_SAVE;
 
 public class PerfProfileSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
@@ -182,7 +185,8 @@ public class PerfProfileSettings extends SettingsPreferenceFragment
             return;
         }
 
-        PerformanceProfile profile = mPerf.getActivePowerProfile();
+        PerformanceProfile profile = mPowerManager.isPowerSaveMode() ?
+                mPerf.getPowerProfile(PROFILE_POWER_SAVE) : mPerf.getActivePowerProfile();
         mPerfSeekBar.setProgress(mProfiles.indexOf(profile));
         mPerfSeekBar.setTitle(getResources().getString(
                 R.string.perf_profile_title, profile.getName()));
@@ -254,6 +258,9 @@ public class PerfProfileSettings extends SettingsPreferenceFragment
 
     private void updatePowerSaveValue() {
         mPowerSavePref.setChecked(mPowerManager.isPowerSaveMode());
+        updatePerfSettings();
+        // The profile was changed automatically without updating the preference
+        PartsUpdater.notifyChanged(getActivity(), getPreferenceScreen().getKey());
     }
 
     private void updateAutoPowerSaveValue() {
@@ -272,8 +279,12 @@ public class PerfProfileSettings extends SettingsPreferenceFragment
     public static final SummaryProvider SUMMARY_PROVIDER = new SummaryProvider() {
         @Override
         public String getSummary(Context context, String key) {
-            final PerformanceProfile profile =
-                    PerformanceManager.getInstance(context).getActivePowerProfile();
+            final PowerManager powerManager =
+                    (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            final PerformanceManager perfManager = PerformanceManager.getInstance(context);
+            final PerformanceProfile profile = powerManager.isPowerSaveMode() ?
+                    perfManager.getPowerProfile(PROFILE_POWER_SAVE) :
+                    perfManager.getActivePowerProfile();
             String summary = context.getString(R.string.perf_profile_settings_summary);
             if (profile != null) {
                 summary += "\n\n" + context.getResources().getString(