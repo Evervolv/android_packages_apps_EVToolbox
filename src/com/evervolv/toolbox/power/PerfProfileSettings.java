LineageOS/android_packages_apps_LineageParts

LineageParts: Use StopMotionVectorDrawable from lineage-sdk
*) It's been moved from uicommon to lineage-sdk internal
   (to allow uicommon to be deprecated).

Change-Id: I6b1de074660d8fc6b8c8c0c0250d6fe1bb9ca9bc

src/org/lineageos/lineageparts/power/PerfProfileSettings.java
@@ -39,7 +39,7 @@ import org.lineageos.lineageparts.PartsUpdater;
 import org.lineageos.lineageparts.R;
 import org.lineageos.lineageparts.widget.SeekBarPreference;
 import org.lineageos.lineageparts.SettingsPreferenceFragment;
-import org.lineageos.graphics.drawable.StopMotionVectorDrawable;
+import org.lineageos.internal.graphics.drawable.StopMotionVectorDrawable;
 
 import java.util.ArrayList;
 import java.util.List;

Evervolv/android_packages_apps_EVToolbox

src/com/evervolv/toolbox/power/PerfProfileSettings.java
@@ -35,7 +35,7 @@ import android.support.v7.preference.Preference;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Toast;
 
-import evervolv.graphics.drawable.StopMotionVectorDrawable;
+import com.evervolv.internal.graphics.drawable.StopMotionVectorDrawable;
 
 import com.evervolv.toolbox.PartsUpdater;
 import com.evervolv.toolbox.R;