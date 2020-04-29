08423dfbf9fb08ef41bc31baea04503ff08b7723
Bruno Martins <bgcngm@gmail.com>
2020-04-29 20:24:20 +0100

    TouchscreenGestureSettings: Hide from search results if unsupported
    Not all devices support touchscreen gestures, so hide the preferences
    from the settings search results if that's the case.
    
    Change-Id: If25ed3f92c330d8d4abf4696af0ee65c94ffa818

--- src/com/evervolv/toolbox/gestures/TouchscreenGestureSettings.java
+++ src/com/evervolv/toolbox/gestures/TouchscreenGestureSettings.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod project
- *               2017 The LineageOS Project
+ *               2017,2019-2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +22,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.util.ArraySet;
+import android.util.Log;
+
 import androidx.preference.ListPreference;
 import androidx.preference.PreferenceManager;
 
@@ -31,11 +34,20 @@ import evervolv.hardware.TouchscreenGesture;
 import com.evervolv.internal.util.ResourceUtils;
 import com.evervolv.toolbox.R;
 import com.evervolv.toolbox.SettingsPreferenceFragment;
+import com.evervolv.toolbox.search.BaseSearchIndexProvider;
+import com.evervolv.toolbox.search.Searchable;
 
 import java.lang.System;
+import java.util.Set;
 
-public class TouchscreenGestureSettings extends SettingsPreferenceFragment {
+public class TouchscreenGestureSettings extends SettingsPreferenceFragment
+        implements Searchable {
+
     private static final String KEY_TOUCHSCREEN_GESTURE = "touchscreen_gesture";
+    private static final String KEY_TOUCHSCREEN_GESTURE_SETTINGS =
+            "touchscreen_gesture_settings";
+    private static final String KEY_TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK =
+            "touchscreen_gesture_haptic_feedback";
     private static final String TOUCHSCREEN_GESTURE_TITLE = KEY_TOUCHSCREEN_GESTURE + "_%s_title";
 
     private TouchscreenGesture[] mTouchscreenGestures;
@@ -199,4 +211,19 @@ public class TouchscreenGestureSettings extends SettingsPreferenceFragment {
         intent.setFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY);
         context.sendBroadcastAsUser(intent, UserHandle.CURRENT);
     }
+
+    public static final Searchable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+
+        @Override
+        public Set<String> getNonIndexableKeys(Context context) {
+            final Set<String> result = new ArraySet<String>();
+
+            if (!isTouchscreenGesturesSupported(context)) {
+                result.add(KEY_TOUCHSCREEN_GESTURE_SETTINGS);
+                result.add(KEY_TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK);
+            }
+            return result;
+        }
+    };
 }
