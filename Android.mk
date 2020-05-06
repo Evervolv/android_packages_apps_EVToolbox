c096a0fbbcbec63454c0953afd2aaad04c18d9fe
Michael Bestas <mkbestas@lineageos.org>
2020-05-06 20:35:19 +0300

    Toolbox: Add privapp permissions xml
    Change-Id: Idf41d848cd2f056e9f3cb79eb7191c779300b564

--- Android.mk
+++ Android.mk
@@ -28,6 +28,7 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+LOCAL_REQUIRED_MODULES := privapp_whitelist_com.evervolv.toolbox.xml
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
@@ -39,3 +40,11 @@ endif
 include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_com.evervolv.toolbox.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
