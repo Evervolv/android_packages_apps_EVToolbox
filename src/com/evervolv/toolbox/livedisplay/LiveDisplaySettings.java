LineageOS/android_packages_apps_LineageParts

LineageParts: Allow removing livedisplay pref screen with config_enableLiveDisplay
* We can't inject via IA if we want this.

Change-Id: I17103ad116d1d26855fb47fdee1fa6cdc914e053

src/org/lineageos/lineageparts/livedisplay/LiveDisplaySettings.java
@@ -397,6 +397,12 @@ public class LiveDisplaySettings extends SettingsPreferenceFragment implements S
             if (!config.hasFeature(FEATURE_READING_ENHANCEMENT)) {
                 result.add(KEY_LIVE_DISPLAY_READING_ENHANCEMENT);
             }
+            if (!context.getResources().getBoolean(
+                    org.lineageos.platform.internal.R.bool.config_enableLiveDisplay)) {
+                result.add(KEY_LIVE_DISPLAY_TEMPERATURE);
+                result.add(KEY_LIVE_DISPLAY);
+            }
+
             return result;
         }
 

Evervolv/android_packages_apps_EVToolbox

src/com/evervolv/toolbox/livedisplay/LiveDisplaySettings.java
@@ -408,6 +408,12 @@ public class LiveDisplaySettings extends SettingsPreferenceFragment implements S
                 }
                 result.add(KEY_LIVE_DISPLAY_TEMPERATURE);
             }
+            if (!context.getResources().getBoolean(
+                    com.evervolv.platform.internal.R.bool.config_enableLiveDisplay)) {
+                result.add(KEY_LIVE_DISPLAY_TEMPERATURE);
+                result.add(KEY_LIVE_DISPLAY);
+            }
+
             return result;
         }
 