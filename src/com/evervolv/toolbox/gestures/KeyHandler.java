LineageOS/android_packages_apps_LineageParts

LineageParts: Fix line lengths
Change-Id: I09bced5c0fa5cce1717e4656ff209b01419eb36c

src/org/lineageos/lineageparts/gestures/KeyHandler.java
@@ -1,6 +1,6 @@
-/**
+/*
  * Copyright (C) 2016 The CyanogenMod project
- *               2017-2020 The LineageOS Project
+ *               2017-2022 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -124,7 +124,8 @@ public class KeyHandler implements DeviceKeyHandler {
             mProximityTimeOut = resources.getInteger(
                     org.lineageos.platform.internal.R.integer.config_proximityCheckTimeout);
             mDefaultProximity = mContext.getResources().getBoolean(
-                    org.lineageos.platform.internal.R.bool.config_proximityCheckOnWakeEnabledByDefault);
+                    org.lineageos.platform.internal.R.bool.
+                            config_proximityCheckOnWakeEnabledByDefault);
 
             mSensorManager = context.getSystemService(SensorManager.class);
             mProximitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
@@ -157,7 +158,8 @@ public class KeyHandler implements DeviceKeyHandler {
 
         if (action != 0 && !mEventHandler.hasMessages(GESTURE_REQUEST)) {
             final Message msg = getMessageForAction(action);
-            final boolean proxWakeEnabled = LineageSettings.System.getInt(mContext.getContentResolver(),
+            final boolean proxWakeEnabled = LineageSettings.System.getInt(
+                    mContext.getContentResolver(),
                     LineageSettings.System.PROXIMITY_ON_WAKE, mDefaultProximity ? 1 : 0) == 1;
             if (mProximityWakeSupported && proxWakeEnabled && mProximitySensor != null) {
                 mGestureWakeLock.acquire(2 * mProximityTimeOut);

src/org/lineageos/lineageparts/input/ButtonBacklightBrightness.java
@@ -256,7 +256,8 @@ public class ButtonBacklightBrightness extends CustomDialogPreference<AlertDialo
 
     private int getTimeout() {
         return LineageSettings.Secure.getInt(mResolver,
-                LineageSettings.Secure.BUTTON_BACKLIGHT_TIMEOUT, DEFAULT_BUTTON_TIMEOUT * 1000) / 1000;
+                LineageSettings.Secure.BUTTON_BACKLIGHT_TIMEOUT, DEFAULT_BUTTON_TIMEOUT * 1000)
+                / 1000;
     }
 
     private void applyTimeout(int timeout) {

src/org/lineageos/lineageparts/notificationlight/BatteryLightSettings.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2012 The CyanogenMod Project
- *               2017-2021 The LineageOS Project
+ *               2017-2022 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -86,10 +86,10 @@ public class BatteryLightSettings extends SettingsPreferenceFragment implements
                 LightsCapabilities.supports(context, LightsCapabilities.LIGHTS_RGB_BATTERY_LED);
         mHALAdjustableBrightness = LightsCapabilities.supports(
                 context, LightsCapabilities.LIGHTS_ADJUSTABLE_BATTERY_LED_BRIGHTNESS);
-        final boolean pulsatingLed =
-                LightsCapabilities.supports(context, LightsCapabilities.LIGHTS_PULSATING_LED);
-        final boolean segmentedBatteryLed =
-                LightsCapabilities.supports(context, LightsCapabilities.LIGHTS_SEGMENTED_BATTERY_LED);
+        final boolean pulsatingLed = LightsCapabilities.supports(context,
+                LightsCapabilities.LIGHTS_PULSATING_LED);
+        final boolean segmentedBatteryLed = LightsCapabilities.supports(context,
+                LightsCapabilities.LIGHTS_SEGMENTED_BATTERY_LED);
 
         addPreferencesFromResource(R.xml.battery_light_settings);
         getActivity().getActionBar().setTitle(R.string.battery_light_title);
@@ -210,11 +210,14 @@ public class BatteryLightSettings extends SettingsPreferenceFragment implements
         ContentResolver resolver = getActivity().getContentResolver();
 
         if (key.equals(LOW_COLOR_PREF)) {
-            LineageSettings.System.putInt(resolver, LineageSettings.System.BATTERY_LIGHT_LOW_COLOR, color);
+            LineageSettings.System.putInt(resolver,
+                    LineageSettings.System.BATTERY_LIGHT_LOW_COLOR, color);
         } else if (key.equals(MEDIUM_COLOR_PREF)) {
-            LineageSettings.System.putInt(resolver, LineageSettings.System.BATTERY_LIGHT_MEDIUM_COLOR, color);
+            LineageSettings.System.putInt(resolver,
+                    LineageSettings.System.BATTERY_LIGHT_MEDIUM_COLOR, color);
         } else if (key.equals(FULL_COLOR_PREF)) {
-            LineageSettings.System.putInt(resolver, LineageSettings.System.BATTERY_LIGHT_FULL_COLOR, color);
+            LineageSettings.System.putInt(resolver,
+                    LineageSettings.System.BATTERY_LIGHT_FULL_COLOR, color);
             updateBrightnessPrefColor(color);
         }
     }

src/org/lineageos/lineageparts/notificationlight/NotificationLightSettings.java
@@ -116,8 +116,8 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
         PreferenceGroup mGeneralPrefs = prefSet.findPreference(GENERAL_SECTION);
 
         // Get the system defined default notification color
-        mDefaultColor =
-                resources.getColor(com.android.internal.R.color.config_defaultNotificationColor, null);
+        mDefaultColor = resources.getColor(
+                com.android.internal.R.color.config_defaultNotificationColor, null);
 
         mDefaultLedOn = resources.getInteger(
                 com.android.internal.R.integer.config_defaultNotificationLedOn);
@@ -197,7 +197,8 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
             mGeneralPrefs.removePreference(mAutoGenerateColors);
         } else {
             mAutoGenerateColors.setOnPreferenceChangeListener(this);
-            watch(LineageSettings.System.getUriFor(LineageSettings.System.NOTIFICATION_LIGHT_COLOR_AUTO));
+            watch(LineageSettings.System.getUriFor(
+                    LineageSettings.System.NOTIFICATION_LIGHT_COLOR_AUTO));
         }
 
         watch(Settings.System.getUriFor(Settings.System.NOTIFICATION_LIGHT_PULSE));
@@ -374,7 +375,8 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
     }
 
     private boolean parsePackageList() {
-        final String baseString = LineageSettings.System.getString(getActivity().getContentResolver(),
+        final String baseString = LineageSettings.System.getString(
+                getActivity().getContentResolver(),
                 LineageSettings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES);
 
         if (TextUtils.equals(mPackageList, baseString)) {
@@ -412,7 +414,7 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
             mPackageList = value;
         }
         LineageSettings.System.putString(getActivity().getContentResolver(),
-                                  LineageSettings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES, value);
+                LineageSettings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES, value);
     }
 
     /**
@@ -423,25 +425,35 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
      * @param timeon
      * @param timeoff
      */
-    protected void updateValues(String packageName, Integer color, Integer timeon, Integer timeoff) {
+    protected void updateValues(String packageName, Integer color, Integer timeon,
+                                Integer timeoff) {
         ContentResolver resolver = getActivity().getContentResolver();
 
         if (packageName.equals(DEFAULT_PREF)) {
-            LineageSettings.System.putInt(resolver, LineageSettings.System.NOTIFICATION_LIGHT_PULSE_DEFAULT_COLOR, color);
-            LineageSettings.System.putInt(resolver, LineageSettings.System.NOTIFICATION_LIGHT_PULSE_DEFAULT_LED_ON, timeon);
-            LineageSettings.System.putInt(resolver, LineageSettings.System.NOTIFICATION_LIGHT_PULSE_DEFAULT_LED_OFF, timeoff);
+            LineageSettings.System.putInt(resolver,
+                    LineageSettings.System.NOTIFICATION_LIGHT_PULSE_DEFAULT_COLOR, color);
+            LineageSettings.System.putInt(resolver,
+                    LineageSettings.System.NOTIFICATION_LIGHT_PULSE_DEFAULT_LED_ON, timeon);
+            LineageSettings.System.putInt(resolver,
+                    LineageSettings.System.NOTIFICATION_LIGHT_PULSE_DEFAULT_LED_OFF, timeoff);
             refreshDefault();
             return;
         } else if (packageName.equals(MISSED_CALL_PREF)) {
-            LineageSettings.System.putInt(resolver, LineageSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_COLOR, color);
-            LineageSettings.System.putInt(resolver, LineageSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_LED_ON, timeon);
-            LineageSettings.System.putInt(resolver, LineageSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_LED_OFF, timeoff);
+            LineageSettings.System.putInt(resolver,
+                    LineageSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_COLOR, color);
+            LineageSettings.System.putInt(resolver,
+                    LineageSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_LED_ON, timeon);
+            LineageSettings.System.putInt(resolver,
+                    LineageSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_LED_OFF, timeoff);
             refreshDefault();
             return;
         } else if (packageName.equals(VOICEMAIL_PREF)) {
-            LineageSettings.System.putInt(resolver, LineageSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_COLOR, color);
-            LineageSettings.System.putInt(resolver, LineageSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_LED_ON, timeon);
-            LineageSettings.System.putInt(resolver, LineageSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_LED_OFF, timeoff);
+            LineageSettings.System.putInt(resolver,
+                    LineageSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_COLOR, color);
+            LineageSettings.System.putInt(resolver,
+                    LineageSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_LED_ON, timeon);
+            LineageSettings.System.putInt(resolver,
+                    LineageSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_LED_OFF, timeoff);
             refreshDefault();
             return;
         }
@@ -460,9 +472,12 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
         ContentResolver resolver = getActivity().getContentResolver();
 
         // Reset to the framework default colors
-        LineageSettings.System.putInt(resolver, LineageSettings.System.NOTIFICATION_LIGHT_PULSE_DEFAULT_COLOR, mDefaultColor);
-        LineageSettings.System.putInt(resolver, LineageSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_COLOR, mDefaultColor);
-        LineageSettings.System.putInt(resolver, LineageSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_COLOR, mDefaultColor);
+        LineageSettings.System.putInt(resolver,
+                LineageSettings.System.NOTIFICATION_LIGHT_PULSE_DEFAULT_COLOR, mDefaultColor);
+        LineageSettings.System.putInt(resolver,
+                LineageSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_COLOR, mDefaultColor);
+        LineageSettings.System.putInt(resolver,
+                LineageSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_COLOR, mDefaultColor);
 
         refreshDefault();
     }

Evervolv/android_packages_apps_EVToolbox

src/com/evervolv/toolbox/gestures/KeyHandler.java
@@ -1,6 +1,6 @@
-/**
+/*
  * Copyright (C) 2016 The CyanogenMod project
- *               2017-2020 The LineageOS Project
+ *               2017-2022 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -123,7 +123,8 @@ public class KeyHandler implements DeviceKeyHandler {
             mProximityTimeOut = resources.getInteger(
                     com.evervolv.platform.internal.R.integer.config_proximityCheckTimeout);
             mDefaultProximity = mContext.getResources().getBoolean(
-                    com.evervolv.platform.internal.R.bool.config_proximityCheckOnWakeEnabledByDefault);
+                    com.evervolv.platform.internal.R.bool.
+                            config_proximityCheckOnWakeEnabledByDefault);
 
             mSensorManager = context.getSystemService(SensorManager.class);
             mProximitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
@@ -156,7 +157,8 @@ public class KeyHandler implements DeviceKeyHandler {
 
         if (action != 0 && !mEventHandler.hasMessages(GESTURE_REQUEST)) {
             final Message msg = getMessageForAction(action);
-            final boolean proxWakeEnabled = EVSettings.System.getInt(mContext.getContentResolver(),
+            final boolean proxWakeEnabled = EVSettings.System.getInt(
+                    mContext.getContentResolver(),
                     EVSettings.System.PROXIMITY_ON_WAKE, mDefaultProximity ? 1 : 0) == 1;
             if (mProximityWakeSupported && proxWakeEnabled && mProximitySensor != null) {
                 mGestureWakeLock.acquire(2 * mProximityTimeOut);

src/com/evervolv/toolbox/input/ButtonBacklightBrightness.java
@@ -217,7 +217,8 @@ public class ButtonBacklightBrightness extends CustomDialogPreferenceExt impleme
 
     private int getTimeout() {
         return EVSettings.System.getInt(mResolver,
-                EVSettings.System.BUTTON_BACKLIGHT_TIMEOUT, DEFAULT_BUTTON_TIMEOUT * 1000) / 1000;
+                EVSettings.System.BUTTON_BACKLIGHT_TIMEOUT, DEFAULT_BUTTON_TIMEOUT * 1000)
+                / 1000;
     }
 
     private void applyTimeout(int timeout) {

src/com/evervolv/toolbox/notificationlight/BatteryLightSettings.java
@@ -82,10 +82,10 @@ public class BatteryLightSettings extends SettingsPreferenceFragment implements
                 LightsCapabilities.supports(context, LightsCapabilities.LIGHTS_RGB_BATTERY_LED);
         mHALAdjustableBrightness = LightsCapabilities.supports(
                 context, LightsCapabilities.LIGHTS_ADJUSTABLE_BATTERY_LED_BRIGHTNESS);
-        final boolean pulsatingLed =
-                LightsCapabilities.supports(context, LightsCapabilities.LIGHTS_PULSATING_LED);
-        final boolean segmentedBatteryLed =
-                LightsCapabilities.supports(context, LightsCapabilities.LIGHTS_SEGMENTED_BATTERY_LED);
+        final boolean pulsatingLed = LightsCapabilities.supports(context,
+                LightsCapabilities.LIGHTS_PULSATING_LED);
+        final boolean segmentedBatteryLed = LightsCapabilities.supports(context,
+                LightsCapabilities.LIGHTS_SEGMENTED_BATTERY_LED);
 
         addPreferencesFromResource(R.xml.battery_light_settings);
         getActivity().getActionBar().setTitle(R.string.battery_light_title);
@@ -207,11 +207,14 @@ public class BatteryLightSettings extends SettingsPreferenceFragment implements
         ContentResolver resolver = getActivity().getContentResolver();
 
         if (key.equals(LOW_COLOR_PREF)) {
-            EVSettings.System.putInt(resolver, EVSettings.System.BATTERY_LIGHT_LOW_COLOR, color);
+            EVSettings.System.putInt(resolver,
+                    EVSettings.System.BATTERY_LIGHT_LOW_COLOR, color);
         } else if (key.equals(MEDIUM_COLOR_PREF)) {
-            EVSettings.System.putInt(resolver, EVSettings.System.BATTERY_LIGHT_MEDIUM_COLOR, color);
+            EVSettings.System.putInt(resolver,
+                    EVSettings.System.BATTERY_LIGHT_MEDIUM_COLOR, color);
         } else if (key.equals(FULL_COLOR_PREF)) {
-            EVSettings.System.putInt(resolver, EVSettings.System.BATTERY_LIGHT_FULL_COLOR, color);
+            EVSettings.System.putInt(resolver,
+                    EVSettings.System.BATTERY_LIGHT_FULL_COLOR, color);
             updateBrightnessPrefColor(color);
         }
     }

src/com/evervolv/toolbox/notificationlight/NotificationLightSettings.java
@@ -120,8 +120,8 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
         PreferenceGroup mGeneralPrefs = (PreferenceGroup) prefSet.findPreference(GENERAL_SECTION);
 
         // Get the system defined default notification color
-        mDefaultColor =
-                resources.getColor(com.android.internal.R.color.config_defaultNotificationColor, null);
+        mDefaultColor = resources.getColor(
+                com.android.internal.R.color.config_defaultNotificationColor, null);
 
         mDefaultLedOn = resources.getInteger(
                 com.android.internal.R.integer.config_defaultNotificationLedOn);
@@ -197,7 +197,8 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
             mGeneralPrefs.removePreference(mAutoGenerateColors);
         } else {
             mAutoGenerateColors.setOnPreferenceChangeListener(this);
-            watch(EVSettings.System.getUriFor(EVSettings.System.NOTIFICATION_LIGHT_COLOR_AUTO));
+            watch(EVSettings.System.getUriFor(
+                    EVSettings.System.NOTIFICATION_LIGHT_COLOR_AUTO));
         }
 
         watch(Settings.System.getUriFor(Settings.System.NOTIFICATION_LIGHT_PULSE));
@@ -365,7 +366,8 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
     }
 
     private boolean parsePackageList() {
-        final String baseString = EVSettings.System.getString(getActivity().getContentResolver(),
+        final String baseString = EVSettings.System.getString(
+                getActivity().getContentResolver(),
                 EVSettings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES);
 
         if (TextUtils.equals(mPackageList, baseString)) {
@@ -401,7 +403,7 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
             mPackageList = value;
         }
         EVSettings.System.putString(getActivity().getContentResolver(),
-                                  EVSettings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES, value);
+                EVSettings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES, value);
     }
 
     /**
@@ -412,25 +414,35 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
      * @param timeon
      * @param timeoff
      */
-    protected void updateValues(String packageName, Integer color, Integer timeon, Integer timeoff) {
+    protected void updateValues(String packageName, Integer color, Integer timeon,
+                                Integer timeoff) {
         ContentResolver resolver = getActivity().getContentResolver();
 
         if (packageName.equals(DEFAULT_PREF)) {
-            EVSettings.System.putInt(resolver, EVSettings.System.NOTIFICATION_LIGHT_PULSE_DEFAULT_COLOR, color);
-            EVSettings.System.putInt(resolver, EVSettings.System.NOTIFICATION_LIGHT_PULSE_DEFAULT_LED_ON, timeon);
-            EVSettings.System.putInt(resolver, EVSettings.System.NOTIFICATION_LIGHT_PULSE_DEFAULT_LED_OFF, timeoff);
+            EVSettings.System.putInt(resolver,
+                    EVSettings.System.NOTIFICATION_LIGHT_PULSE_DEFAULT_COLOR, color);
+            EVSettings.System.putInt(resolver,
+                    EVSettings.System.NOTIFICATION_LIGHT_PULSE_DEFAULT_LED_ON, timeon);
+            EVSettings.System.putInt(resolver,
+                    EVSettings.System.NOTIFICATION_LIGHT_PULSE_DEFAULT_LED_OFF, timeoff);
             refreshDefault();
             return;
         } else if (packageName.equals(MISSED_CALL_PREF)) {
-            EVSettings.System.putInt(resolver, EVSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_COLOR, color);
-            EVSettings.System.putInt(resolver, EVSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_LED_ON, timeon);
-            EVSettings.System.putInt(resolver, EVSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_LED_OFF, timeoff);
+            EVSettings.System.putInt(resolver,
+                    EVSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_COLOR, color);
+            EVSettings.System.putInt(resolver,
+                    EVSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_LED_ON, timeon);
+            EVSettings.System.putInt(resolver,
+                    EVSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_LED_OFF, timeoff);
             refreshDefault();
             return;
         } else if (packageName.equals(VOICEMAIL_PREF)) {
-            EVSettings.System.putInt(resolver, EVSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_COLOR, color);
-            EVSettings.System.putInt(resolver, EVSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_LED_ON, timeon);
-            EVSettings.System.putInt(resolver, EVSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_LED_OFF, timeoff);
+            EVSettings.System.putInt(resolver,
+                    EVSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_COLOR, color);
+            EVSettings.System.putInt(resolver,
+                    EVSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_LED_ON, timeon);
+            EVSettings.System.putInt(resolver,
+                    EVSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_LED_OFF, timeoff);
             refreshDefault();
             return;
         }
@@ -449,9 +461,12 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
         ContentResolver resolver = getActivity().getContentResolver();
 
         // Reset to the framework default colors
-        EVSettings.System.putInt(resolver, EVSettings.System.NOTIFICATION_LIGHT_PULSE_DEFAULT_COLOR, mDefaultColor);
-        EVSettings.System.putInt(resolver, EVSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_COLOR, mDefaultColor);
-        EVSettings.System.putInt(resolver, EVSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_COLOR, mDefaultColor);
+        EVSettings.System.putInt(resolver,
+                EVSettings.System.NOTIFICATION_LIGHT_PULSE_DEFAULT_COLOR, mDefaultColor);
+        EVSettings.System.putInt(resolver,
+                EVSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_COLOR, mDefaultColor);
+        EVSettings.System.putInt(resolver,
+                EVSettings.System.NOTIFICATION_LIGHT_PULSE_VMAIL_COLOR, mDefaultColor);
 
         refreshDefault();
     }