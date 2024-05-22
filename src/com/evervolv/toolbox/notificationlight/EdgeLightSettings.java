455143451f7a8609c48b17767355e15e6f1488a8
Pranav Vashi <neobuddy89@gmail.com>
2024-05-22 16:55:57 +0900

    Toolbox: Add edge light customizations [2/2]
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Change-Id: I9966d557307197e9d758ccc0ea1a35297e9eaf83

---
+++ src/com/evervolv/toolbox/notificationlight/EdgeLightSettings.java
@@ -0,0 +1,22 @@
+/*
+ * SPDX-FileCopyrightText: 2016 The CyanogenMod project
+ * SPDX-FileCopyrightText: 2017,2019-2020 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.evervolv.toolbox.notificationlight;
+
+import android.os.Bundle;
+
+import com.evervolv.toolbox.R;
+import com.evervolv.toolbox.SettingsPreferenceFragment;
+
+public class EdgeLightSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.edge_light_settings);
+    }
+}
