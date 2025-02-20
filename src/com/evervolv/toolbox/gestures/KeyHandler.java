LineageOS/android_packages_apps_LineageParts

LineageParts: Fix a few warnings
* Wakelocks should be "Tag:Name" according to AS
* Use PreferenceFragmentCompat instead of PreferenceFragment
* title and therefore whole check sequence is unused

Change-Id: Iaf1c7ec38319269d9a6c12efb7f959a93245b13d

src/org/lineageos/lineageparts/gestures/KeyHandler.java
@@ -108,7 +108,7 @@ public class KeyHandler implements DeviceKeyHandler {
 
         mPowerManager = context.getSystemService(PowerManager.class);
         mGestureWakeLock = mPowerManager.newWakeLock(
-                PowerManager.PARTIAL_WAKE_LOCK, "LineagePartsGestureWakeLock");
+                PowerManager.PARTIAL_WAKE_LOCK, "LineageParts:GestureWakeLock");
 
         mEventHandler = new EventHandler(Looper.getMainLooper());
 
@@ -131,10 +131,11 @@ public class KeyHandler implements DeviceKeyHandler {
             mSensorManager = context.getSystemService(SensorManager.class);
             mProximitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
             mProximityWakeLock = mPowerManager.newWakeLock(
-                    PowerManager.PARTIAL_WAKE_LOCK, "LineagePartsProximityWakeLock");
+                    PowerManager.PARTIAL_WAKE_LOCK, "LineageParts:ProximityWakeLock");
         }
         mContext.registerReceiver(mUpdateReceiver,
-                new IntentFilter(TouchscreenGestureConstants.UPDATE_PREFS_ACTION));
+                new IntentFilter(TouchscreenGestureConstants.UPDATE_PREFS_ACTION),
+                Context.RECEIVER_NOT_EXPORTED);
     }
 
     private class TorchModeCallback extends CameraManager.TorchCallback {

Evervolv/android_packages_apps_EVToolbox

  title and therefore whole check sequence is unused

src/com/evervolv/toolbox/gestures/KeyHandler.java
@@ -96,7 +96,7 @@ public class KeyHandler implements DeviceKeyHandler {
 
         mPowerManager = context.getSystemService(PowerManager.class);
         mGestureWakeLock = mPowerManager.newWakeLock(
-                PowerManager.PARTIAL_WAKE_LOCK, "SettingsGestureWakeLock");
+                PowerManager.PARTIAL_WAKE_LOCK, "Settings:GestureWakeLock");
 
         mEventHandler = new EventHandler(Looper.getMainLooper());
 
@@ -119,10 +119,11 @@ public class KeyHandler implements DeviceKeyHandler {
             mSensorManager = context.getSystemService(SensorManager.class);
             mProximitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
             mProximityWakeLock = mPowerManager.newWakeLock(
-                    PowerManager.PARTIAL_WAKE_LOCK, "SettingsProximityWakeLock");
+                    PowerManager.PARTIAL_WAKE_LOCK, "Settings:ProximityWakeLock");
         }
         mContext.registerReceiver(mUpdateReceiver,
-                new IntentFilter(TouchscreenGestureConstants.UPDATE_PREFS_ACTION));
+                new IntentFilter(TouchscreenGestureConstants.UPDATE_PREFS_ACTION),
+                Context.RECEIVER_NOT_EXPORTED);
     }
 
     private class TorchModeCallback extends CameraManager.TorchCallback {