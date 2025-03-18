LineageOS/android_packages_apps_LineageParts

LineageParts: Support breath LED mode
Change-Id: Ic49a4ae260294aeb10235ca6089c9169d4b2fd41

src/org/lineageos/lineageparts/notificationlight/ApplicationLightPreference.java
@@ -22,6 +22,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.preference.PreferenceViewHolder;
 
 import org.lineageos.internal.notification.LightsCapabilities;
+import org.lineageos.lineageparts.notificationlight.LightSettingsDialog.OnOffType;
 import org.lineageos.lineageparts.widget.CustomDialogPreference;
 import org.lineageos.lineageparts.R;
 
@@ -39,7 +40,7 @@ public class ApplicationLightPreference extends CustomDialogPreference<LightSett
     private int mColorValue;
     private int mOnValue;
     private int mOffValue;
-    private boolean mOnOffChangeable;
+    private OnOffType mOnOffType;
 
     private boolean mHasDefaults;
     private int mDefaultColorValue;
@@ -62,18 +63,17 @@ public class ApplicationLightPreference extends CustomDialogPreference<LightSett
 
     public ApplicationLightPreference(Context context, AttributeSet attrs,
                                       int color, int onValue, int offValue) {
-        this(context, attrs, color, onValue, offValue,
-                LightsCapabilities.supports(context, LightsCapabilities.LIGHTS_PULSATING_LED));
+        this(context, attrs, color, onValue, offValue, getOnOffType(context));
     }
 
     public ApplicationLightPreference(Context context, AttributeSet attrs,
                                       int color, int onValue, int offValue,
-                                      boolean onOffChangeable) {
+                                      OnOffType onOffType) {
         super(context, attrs);
         mColorValue = color;
         mOnValue = onValue;
         mOffValue = offValue;
-        mOnOffChangeable = onOffChangeable;
+        mOnOffType = onOffType;
         mHasDefaults = false;
         mLedBrightness = 0; // use system brightness
 
@@ -128,6 +128,18 @@ public class ApplicationLightPreference extends CustomDialogPreference<LightSett
         }
     }
 
+    private static OnOffType getOnOffType(Context context) {
+        if (LightsCapabilities.supports(context, LightsCapabilities.LIGHTS_PULSATING_LED)) {
+            return OnOffType.PULSE;
+        }
+
+        if (LightsCapabilities.supports(context, LightsCapabilities.LIGHTS_BREATHING_LED)) {
+            return OnOffType.BREATH;
+        }
+
+        return OnOffType.TOGGLE;
+    }
+
     private void updatePreferenceViews() {
         final int size = (int) getContext().getResources().getDimension(
                 R.dimen.oval_notification_size);
@@ -144,7 +156,7 @@ public class ApplicationLightPreference extends CustomDialogPreference<LightSett
             mOnValueView.setText(mapLengthValue(mOnValue));
         }
         if (mOffValueView != null) {
-            if (mOnValue == 1 || !mOnOffChangeable) {
+            if (mOnValue == 1 || mOnOffType == OnOffType.TOGGLE) {
                 mOffValueView.setVisibility(View.GONE);
             } else {
                 mOffValueView.setVisibility(View.VISIBLE);
@@ -158,7 +170,7 @@ public class ApplicationLightPreference extends CustomDialogPreference<LightSett
         if (which == DialogInterface.BUTTON_NEUTRAL) {
             // Reset to previously supplied defaults
             mDialog.setColor(mDefaultColorValue);
-            if (mOnOffChangeable) {
+            if (mOnOffType != OnOffType.TOGGLE) {
                 mDialog.setPulseSpeedOn(mDefaultOnValue);
                 mDialog.setPulseSpeedOff(mDefaultOffValue);
             }
@@ -182,7 +194,7 @@ public class ApplicationLightPreference extends CustomDialogPreference<LightSett
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         mDialog = new LightSettingsDialog(getContext(), 0xFF000000 | mColorValue,
-                mOnValue, mOffValue, mOnOffChangeable, mLedBrightness);
+                mOnValue, mOffValue, mOnOffType, mLedBrightness);
         mDialog.setAlphaSliderVisible(false);
 
         // Initialize the buttons with null handlers, as they will get remapped by
@@ -230,11 +242,11 @@ public class ApplicationLightPreference extends CustomDialogPreference<LightSett
         updatePreferenceViews();
     }
 
-    public void setAllValues(int color, int onValue, int offValue, boolean onOffChangeable) {
+    public void setAllValues(int color, int onValue, int offValue, OnOffType onOffType) {
         mColorValue = color;
         mOnValue = onValue;
         mOffValue = offValue;
-        mOnOffChangeable = onOffChangeable;
+        mOnOffType = onOffType;
         updatePreferenceViews();
     }
 
@@ -261,7 +273,7 @@ public class ApplicationLightPreference extends CustomDialogPreference<LightSett
     }
 
     private String mapLengthValue(Integer time) {
-        if (!mOnOffChangeable) {
+        if (mOnOffType == OnOffType.TOGGLE) {
             return getContext().getResources().getString(R.string.pulse_length_always_on);
         }
         if (time == DEFAULT_TIME) {

src/org/lineageos/lineageparts/notificationlight/BatteryLightSettings.java
@@ -22,6 +22,7 @@ import androidx.preference.PreferenceScreen;
 import org.lineageos.internal.notification.LightsCapabilities;
 import org.lineageos.lineageparts.R;
 import org.lineageos.lineageparts.SettingsPreferenceFragment;
+import org.lineageos.lineageparts.notificationlight.LightSettingsDialog.OnOffType;
 import org.lineageos.lineageparts.search.BaseSearchIndexProvider;
 import org.lineageos.lineageparts.search.Searchable;
 
@@ -78,8 +79,7 @@ public class BatteryLightSettings extends SettingsPreferenceFragment implements
         // liblights supports brightness control
         final boolean halAdjustableBrightness = LightsCapabilities.supports(context,
                 LightsCapabilities.LIGHTS_ADJUSTABLE_BATTERY_LED_BRIGHTNESS);
-        final boolean pulsatingLed = LightsCapabilities.supports(context,
-                LightsCapabilities.LIGHTS_PULSATING_LED);
+        final boolean pulsatingLed = LightsCapabilities.blinks(context);
         final boolean segmentedBatteryLed = LightsCapabilities.supports(context,
                 LightsCapabilities.LIGHTS_SEGMENTED_BATTERY_LED);
 
@@ -161,19 +161,19 @@ public class BatteryLightSettings extends SettingsPreferenceFragment implements
         if (mLowColorPref != null) {
             int lowColor = LineageSettings.System.getInt(resolver,
                     LineageSettings.System.BATTERY_LIGHT_LOW_COLOR, mDefaultLowColor);
-            mLowColorPref.setAllValues(lowColor, 0, 0, false);
+            mLowColorPref.setAllValues(lowColor, 0, 0, OnOffType.TOGGLE);
         }
 
         if (mMediumColorPref != null) {
             int mediumColor = LineageSettings.System.getInt(resolver,
                     LineageSettings.System.BATTERY_LIGHT_MEDIUM_COLOR, mDefaultMediumColor);
-            mMediumColorPref.setAllValues(mediumColor, 0, 0, false);
+            mMediumColorPref.setAllValues(mediumColor, 0, 0, OnOffType.TOGGLE);
         }
 
         if (mFullColorPref != null) {
             int fullColor = LineageSettings.System.getInt(resolver,
                     LineageSettings.System.BATTERY_LIGHT_FULL_COLOR, mDefaultFullColor);
-            mFullColorPref.setAllValues(fullColor, 0, 0, false);
+            mFullColorPref.setAllValues(fullColor, 0, 0, OnOffType.TOGGLE);
             updateBrightnessPrefColor(fullColor);
         }
     }
@@ -308,7 +308,7 @@ public class BatteryLightSettings extends SettingsPreferenceFragment implements
                 result.add(BRIGHTNESS_PREFERENCE);
                 result.add(BRIGHTNESS_ZEN_PREFERENCE);
             }
-            if (!LightsCapabilities.supports(context, LightsCapabilities.LIGHTS_PULSATING_LED) ||
+            if (!LightsCapabilities.blinks(context) ||
                     LightsCapabilities.supports(context,
                             LightsCapabilities.LIGHTS_SEGMENTED_BATTERY_LED)) {
                 result.add(PULSE_ENABLED_PREF);

Evervolv/android_packages_apps_EVToolbox

src/com/evervolv/toolbox/notificationlight/ApplicationLightPreference.java
@@ -22,6 +22,7 @@ import androidx.preference.PreferenceViewHolder;
 
 import com.evervolv.internal.notification.LightsCapabilities;
 import com.evervolv.settingslib.widget.CustomDialogPreferenceExt;
+import com.evervolv.toolbox.notificationlight.LightSettingsDialog.OnOffType;
 import com.evervolv.toolbox.R;
 
 public class ApplicationLightPreference extends CustomDialogPreferenceExt
@@ -38,7 +39,7 @@ public class ApplicationLightPreference extends CustomDialogPreferenceExt
     private int mColorValue;
     private int mOnValue;
     private int mOffValue;
-    private boolean mOnOffChangeable;
+    private OnOffType mOnOffType;
 
     private boolean mHasDefaults;
     private int mDefaultColorValue;
@@ -61,18 +62,17 @@ public class ApplicationLightPreference extends CustomDialogPreferenceExt
 
     public ApplicationLightPreference(Context context, AttributeSet attrs,
                                       int color, int onValue, int offValue) {
-        this(context, attrs, color, onValue, offValue,
-                LightsCapabilities.supports(context, LightsCapabilities.LIGHTS_PULSATING_LED));
+        this(context, attrs, color, onValue, offValue, getOnOffType(context));
     }
 
     public ApplicationLightPreference(Context context, AttributeSet attrs,
                                       int color, int onValue, int offValue,
-                                      boolean onOffChangeable) {
+                                      OnOffType onOffType) {
         super(context, attrs);
         mColorValue = color;
         mOnValue = onValue;
         mOffValue = offValue;
-        mOnOffChangeable = onOffChangeable;
+        mOnOffType = onOffType;
         mHasDefaults = false;
         mLedBrightness = 0; // use system brightness
 
@@ -127,6 +127,18 @@ public class ApplicationLightPreference extends CustomDialogPreferenceExt
         }
     }
 
+    private static OnOffType getOnOffType(Context context) {
+        if (LightsCapabilities.supports(context, LightsCapabilities.LIGHTS_PULSATING_LED)) {
+            return OnOffType.PULSE;
+        }
+
+        if (LightsCapabilities.supports(context, LightsCapabilities.LIGHTS_BREATHING_LED)) {
+            return OnOffType.BREATH;
+        }
+
+        return OnOffType.TOGGLE;
+    }
+
     private void updatePreferenceViews() {
         final int size = (int) getContext().getResources().getDimension(
                 R.dimen.oval_notification_size);
@@ -143,7 +155,7 @@ public class ApplicationLightPreference extends CustomDialogPreferenceExt
             mOnValueView.setText(mapLengthValue(mOnValue));
         }
         if (mOffValueView != null) {
-            if (mOnValue == 1 || !mOnOffChangeable) {
+            if (mOnValue == 1 || mOnOffType != OnOffType.PULSE) {
                 mOffValueView.setVisibility(View.GONE);
             } else {
                 mOffValueView.setVisibility(View.VISIBLE);
@@ -157,7 +169,7 @@ public class ApplicationLightPreference extends CustomDialogPreferenceExt
         if (which == DialogInterface.BUTTON_NEUTRAL) {
             // Reset to previously supplied defaults
             mDialog.setColor(mDefaultColorValue);
-            if (mOnOffChangeable) {
+            if (mOnOffType != OnOffType.TOGGLE) {
                 mDialog.setPulseSpeedOn(mDefaultOnValue);
                 mDialog.setPulseSpeedOff(mDefaultOffValue);
             }
@@ -181,7 +193,7 @@ public class ApplicationLightPreference extends CustomDialogPreferenceExt
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         mDialog = new LightSettingsDialog(getContext(), 0xFF000000 | mColorValue,
-                mOnValue, mOffValue, mOnOffChangeable, mLedBrightness);
+                mOnValue, mOffValue, mOnOffType, mLedBrightness);
         mDialog.setAlphaSliderVisible(false);
 
         // Initialize the buttons with null handlers, as they will get remapped by
@@ -239,11 +251,11 @@ public class ApplicationLightPreference extends CustomDialogPreferenceExt
         updatePreferenceViews();
     }
 
-    public void setAllValues(int color, int onValue, int offValue, boolean onOffChangeable) {
+    public void setAllValues(int color, int onValue, int offValue, OnOffType onOffType) {
         mColorValue = color;
         mOnValue = onValue;
         mOffValue = offValue;
-        mOnOffChangeable = onOffChangeable;
+        mOnOffType = onOffType;
         updatePreferenceViews();
     }
 
@@ -253,8 +265,8 @@ public class ApplicationLightPreference extends CustomDialogPreferenceExt
         updatePreferenceViews();
     }
 
-    public void setOnOffChangeable(boolean value) {
-        mOnOffChangeable = value;
+    public void setOnOffChangeable(OnOffType onOffType) {
+        mOnOffType = onOffType;
     }
 
     public void setDefaultValues(int color, int onValue, int offValue) {
@@ -280,11 +292,13 @@ public class ApplicationLightPreference extends CustomDialogPreferenceExt
     }
 
     private String mapLengthValue(Integer time) {
-        if (!mOnOffChangeable) {
+        if (mOnOffType == OnOffType.TOGGLE) {
             return getContext().getResources().getString(R.string.pulse_length_always_on);
         }
         if (time == DEFAULT_TIME) {
-            return getContext().getResources().getString(R.string.default_time);
+            return mOnOffType == OnOffType.BREATH
+                    ? getContext().getResources().getString(R.string.pulse_length_blink)
+                    : getContext().getResources().getString(R.string.default_time);
         }
 
         String[] timeNames = getContext().getResources().getStringArray(

src/com/evervolv/toolbox/notificationlight/BatteryLightSettings.java
@@ -22,6 +22,7 @@ import com.evervolv.internal.notification.LightsCapabilities;
 import com.evervolv.settingslib.widget.EVSystemSettingMainSwitchPreference;
 import com.evervolv.toolbox.R;
 import com.evervolv.toolbox.SettingsPreferenceFragment;
+import com.evervolv.toolbox.notificationlight.LightSettingsDialog.OnOffType;
 import com.evervolv.toolbox.search.BaseSearchIndexProvider;
 import com.evervolv.toolbox.search.Searchable;
 
@@ -77,8 +78,7 @@ public class BatteryLightSettings extends SettingsPreferenceFragment implements
                 LightsCapabilities.supports(context, LightsCapabilities.LIGHTS_RGB_BATTERY_LED);
         mHALAdjustableBrightness = LightsCapabilities.supports(
                 context, LightsCapabilities.LIGHTS_ADJUSTABLE_BATTERY_LED_BRIGHTNESS);
-        final boolean pulsatingLed = LightsCapabilities.supports(context,
-                LightsCapabilities.LIGHTS_PULSATING_LED);
+        final boolean blinkingLed = LightsCapabilities.blinks(context);
         final boolean segmentedBatteryLed = LightsCapabilities.supports(context,
                 LightsCapabilities.LIGHTS_SEGMENTED_BATTERY_LED);
 
@@ -106,7 +106,7 @@ public class BatteryLightSettings extends SettingsPreferenceFragment implements
 
         mBatteryBrightness = mBatteryBrightnessPref.getBrightnessSetting();
 
-        if (!pulsatingLed || segmentedBatteryLed) {
+        if (!blinkingLed || segmentedBatteryLed) {
             generalPrefs.removePreference(mPulseEnabledPref);
         }
 
@@ -165,19 +165,19 @@ public class BatteryLightSettings extends SettingsPreferenceFragment implements
         if (mLowColorPref != null) {
             int lowColor = EVSettings.System.getInt(resolver,
                     EVSettings.System.BATTERY_LIGHT_LOW_COLOR, mDefaultLowColor);
-            mLowColorPref.setAllValues(lowColor, 0, 0, false);
+            mLowColorPref.setAllValues(lowColor, 0, 0, OnOffType.TOGGLE);
         }
 
         if (mMediumColorPref != null) {
             int mediumColor = EVSettings.System.getInt(resolver,
                     EVSettings.System.BATTERY_LIGHT_MEDIUM_COLOR, mDefaultMediumColor);
-            mMediumColorPref.setAllValues(mediumColor, 0, 0, false);
+            mMediumColorPref.setAllValues(mediumColor, 0, 0, OnOffType.TOGGLE);
         }
 
         if (mFullColorPref != null) {
             int fullColor = EVSettings.System.getInt(resolver,
                     EVSettings.System.BATTERY_LIGHT_FULL_COLOR, mDefaultFullColor);
-            mFullColorPref.setAllValues(fullColor, 0, 0, false);
+            mFullColorPref.setAllValues(fullColor, 0, 0, OnOffType.TOGGLE);
             updateBrightnessPrefColor(fullColor);
         }
     }
@@ -303,7 +303,7 @@ public class BatteryLightSettings extends SettingsPreferenceFragment implements
                 result.add(BRIGHTNESS_PREFERENCE);
                 result.add(BRIGHTNESS_ZEN_PREFERENCE);
             }
-            if (!LightsCapabilities.supports(context, LightsCapabilities.LIGHTS_PULSATING_LED) ||
+            if (!LightsCapabilities.blinks(context) ||
                     LightsCapabilities.supports(context,
                             LightsCapabilities.LIGHTS_SEGMENTED_BATTERY_LED)) {
                 result.add(PULSE_ENABLED_PREF);